2a65280b01d63c6bf3215d852a76bdab6b4a0f0d
Chris Lewis <me@chrislewis.au>
2024-09-28 18:29:43 -0400

    missing assets

---
+++ patch_linker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Extract the Splat-generated linker script and append the custom patch
+
+# Append the custom definition for D_10D40
+echo "PROVIDE(D_10D40 = entry_BSS_END);" >> snowboardkids2.ld
+
