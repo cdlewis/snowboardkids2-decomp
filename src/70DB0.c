c3facf9c751079bd3427e1499b7595f6b9e407ea
Chris Lewis <me@chrislewis.au>
2025-02-12 19:40:43 -0500

    match function: initialize_video_and_threads

---
+++ src/70DB0.c
@@ -0,0 +1,18 @@
+#include "common.h"
+#include "initialize_video_and_threads.c"
+
+INCLUDE_ASM("asm/nonmatchings/70DB0", thread_function_1);
+
+INCLUDE_ASM("asm/nonmatchings/70DB0", func_800705D0_711D0);
+
+INCLUDE_ASM("asm/nonmatchings/70DB0", func_80070650_71250);
+
+INCLUDE_ASM("asm/nonmatchings/70DB0", thread_function_2);
+
+INCLUDE_ASM("asm/nonmatchings/70DB0", thread_function_3);
+
+INCLUDE_ASM("asm/nonmatchings/70DB0", func_80070990_71590);
+
+INCLUDE_ASM("asm/nonmatchings/70DB0", thread_function_4);
+
+INCLUDE_ASM("asm/nonmatchings/70DB0", func_80070B9C_7179C);
