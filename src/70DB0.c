fcfe831d9f3188d3e57ed74fbc2b889b5f26d50e
Chris Lewis <me@chrislewis.au>
2025-02-15 19:05:37 -0500

    Match thread_function_1

--- src/70DB0.c
+++ src/70DB0.c
@@ -15,7 +15,7 @@ typedef struct viConfig_s ViConfig;
 struct viConfig_s {
     ViConfig **prevConfig;
     ViConfig **nextConfig;
-    int mode;
+    OSMesgQueue *messageQueue;
     u16 frameCounter;
     u16 maxFrames;
 };
@@ -25,6 +25,8 @@ int vertical_retrace_message[3] = {0x5, 0, 0};
 int sp_task_done_message[3] = {0x6, 0, 0};
 int dp_interrupt_message[3] = {0x8, 0, 0};
 int prenmi_interrupt_message[3] = {0xA, 0, 0};
+int D_8009B020_9BC20 = 0;
+int D_8009B024_9BC24 = 0x1E;
 
 // bss
 OSThread thread_a;
@@ -43,7 +45,7 @@ char thread_a_stack[0x180];
 char thread_b_stack[0x180];
 char thread_c_stack[0x180];
 char thread_d_stack[0x180];
-s16 frameCounter;
+u16 frameCounter;
 s16 frameDelay;
 
 // I'm not convinced this is **. A single pointer would
@@ -96,9 +98,70 @@ void initialize_video_and_threads(s32 viMode) {
     osStartThread(&thread_d);
 }
 
-INCLUDE_ASM("asm/nonmatchings/70DB0", thread_function_1);
+void thread_function_1(void *arg) {
+    u32 temp = 0xA << 16;
+    s32 *message;
+    s16 temp_v0;
+    s32 messageType;
+    u32 messagePayload;
+    ViConfig *currentConfig;
+    ViConfig *config;
 
-void addViConfig(ViConfig *config, s32 mode, s32 frameCount) {
+    while (TRUE) {
+        osRecvMesg(&viEventQueue, (OSMesg *)&message, OS_MESG_BLOCK);
+        messageType = *message;
+
+        if (messageType != 0x5) {
+            if (messageType == 0xA) {
+                // run through all configs and send as message until none are left?
+                messagePayload = 0xFFFF & messagePayload;
+                messagePayload = messagePayload | temp;
+                messagePayload = (messagePayload & 0xFFFF0000);
+                messagePayload = messagePayload | frameCounter;
+                currentConfig = currentViConfig;
+                while (currentConfig) {
+                    osSendMesg(currentConfig->messageQueue, (OSMesg)messagePayload, OS_MESG_NOBLOCK);
+                    currentConfig = currentConfig->nextConfig;
+                }
+
+                osViBlack(TRUE);
+                D_8009B020_9BC20 = 1;
+            }
+
+            continue;
+        }
+
+        // Increment frameCounter and wrap if > 0xFFF
+        frameCounter = (frameCounter + 1) & 0xFFF;
+
+        if (D_8009B020_9BC20 && D_8009B024_9BC24) {
+            D_8009B024_9BC24--;
+        }
+
+        messagePayload = messagePayload & 0xFFFF;
+        messagePayload = messagePayload | 0x50000;
+        messagePayload = (messagePayload & 0xFFFF0000);
+        messagePayload = messagePayload | frameCounter;
+        config = currentViConfig;
+        while (config) {
+            temp_v0 = config->frameCounter;
+            if (temp_v0) {
+                config->frameCounter = (s16)(temp_v0 - 1);
+            } else {
+                config->frameCounter = config->maxFrames;
+                osSendMesg(config->messageQueue, (OSMesg)messagePayload, OS_MESG_NOBLOCK);
+            }
+            config = config->nextConfig;
+        }
+
+        if (!frameDelay) {
+            continue;
+        }
+        frameDelay--;
+    }
+}
+
+void addViConfig(ViConfig *config, OSMesgQueue *messageQueue, s32 frameCount) {
     ViConfig *prevConfig;
     u32 nextIntMask;
 
@@ -109,7 +172,7 @@ void addViConfig(ViConfig *config, s32 mode, s32 frameCount) {
     }
     prevConfig = currentViConfig;
     config->prevConfig = NULL;
-    config->mode = mode;
+    config->messageQueue = messageQueue;
     config->frameCounter = 0;
     config->maxFrames = (s16)(frameCount - 1);
     currentViConfig = config;
