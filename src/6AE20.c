322aa765150da3916ecab9bc0f1882a1810516ad
Chris Lewis <me@chrislewis.au>
2025-03-14 21:17:30 -0400

    starting on 6AE20

---
+++ src/6AE20.c
@@ -0,0 +1,19 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/6AE20", func_8006A220_6AE20);
+
+INCLUDE_ASM("asm/nonmatchings/6AE20", func_8006A258_6AE58);
+
+INCLUDE_ASM("asm/nonmatchings/6AE20", func_8006A3FC_6AFFC);
+
+INCLUDE_ASM("asm/nonmatchings/6AE20", func_8006A428_6B028);
+
+INCLUDE_ASM("asm/nonmatchings/6AE20", func_8006A4DC_6B0DC);
+
+INCLUDE_ASM("asm/nonmatchings/6AE20", func_8006A4E8_6B0E8);
+
+INCLUDE_ASM("asm/nonmatchings/6AE20", func_8006A51C_6B11C);
+
+INCLUDE_ASM("asm/nonmatchings/6AE20", func_8006A524_6B124);
+
+INCLUDE_ASM("asm/nonmatchings/6AE20", func_8006A52C_6B12C);
