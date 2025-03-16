790e97ea7cf14f99daf5a5f3118ed8cd38ee8215
Chris Lewis <me@chrislewis.au>
2025-03-15 20:35:16 -0400

    match wall_newspaper_trigger

--- src/wall_newspaper_trigger.c
+++ src/wall_newspaper_trigger.c
@@ -11,32 +11,33 @@ typedef struct {
     s8 unk425;
 } GameState;
 
-void PhoneTriggerCheck(u8 *);
+void func_80069CC0_6A8C0(void *);
+void WallNewspaperTriggerCheck(u8 *);
+
+u8 WallNewspaperLabel[] = "WALL NEWSPAPER";
 
 extern GameState *GameStateGet();
-extern void func_80069CC0_6A8C0(void *);
 extern u8 D_8008D6C4_8E2C4[];
 
-char PhoneLabel[] = "PHONE";
-
-void PhoneTriggerInit(EventTrigger *arg0) {
-    arg0->unk0 = PHONE_EVENT_ID;
+void WallNewspaperTriggerInit(EventTrigger *arg0) {
+    arg0->unk0 = WALL_NEWSPAPER_EVENT_ID;
     arg0->unk6 = -0x68;
     arg0->unk1 = 0;
     arg0->unk4 = 0;
     arg0->unk8 = 0;
-    arg0->unkC = &PhoneLabel;
+    arg0->unkC = &WallNewspaperLabel;
 
-    func_80069CC0_6A8C0(&PhoneTriggerCheck);
+    func_80069CC0_6A8C0(&WallNewspaperTriggerCheck);
 }
 
-void PhoneTriggerCheck(u8 *arg0) {
+void WallNewspaperTriggerCheck(u8 *arg0) {
     s16 temp_v0_2;
     s16 var_v1;
     s32 temp_a2;
     GameState *temp_v0;
     s32 val1;
     s32 val2;
+
     temp_v0 = GameStateGet();
     if (temp_v0->unk3F8 > 0x760000) {
         temp_v0_2 = temp_v0->unk3F4;
@@ -50,10 +51,8 @@ void PhoneTriggerCheck(u8 *arg0) {
             val2 = ((s16 *)D_8008D6C4_8E2C4)[(temp_a2 * 2) + 1];
             if (var_v1 > val2) {
                 u32 temp = temp_v0->unk3FC - 0xC01;
-                if (temp < 0x7FF) {
-                    temp_v0->unk424 = 1;
-                    temp_v0->unk425 = *arg0;
-                }
+                temp_v0->unk424 = 1;
+                temp_v0->unk425 = *arg0;
             }
         }
     }
