4bca3ffd0fcbb66ab6434bbb10fd0486254672db
Chris Lewis <me@chrislewis.au>
2025-03-17 20:11:49 -0400

    match func_8001716C_17D6C

--- src/17AA0.c
+++ src/17AA0.c
@@ -14,12 +14,51 @@ extern void func_8001730C_17F0C();
 extern void func_800172DC_17EDC();
 extern void func_80017328_17F28();
 extern void func_80000760_1360();
+extern void func_80069DD4_6A9D4(s32 a0, s32 a1);
+extern void func_80069D7C_6A97C(s32 a0);
+extern void func_80057564_58164(s32 a0);
+extern void n_alSynRemovePlayer(void *player);
+extern void *func_8006A200_6AE00(void *ptr);
+extern void func_800697CC_6A3CC(void (*func)(void));
+extern void func_80017248_17E48(void);
+extern void func_80017264_17E64(void);
+extern u8 D_800A8CC8_A0038;
 
 INCLUDE_ASM("asm/nonmatchings/17AA0", func_80016EA0_17AA0);
 
 INCLUDE_ASM("asm/nonmatchings/17AA0", func_80017110_17D10);
 
-INCLUDE_ASM("asm/nonmatchings/17AA0", func_8001716C_17D6C);
+void func_8001716C_17D6C(void) {
+    GameState *gs = GameStateGet();
+
+    u8 val = gs->unk42E;
+    if (val != 0) {
+        if (val == 2) {
+            func_80069DD4_6A9D4(2, 1);
+        } else {
+            func_80069D7C_6A97C(2);
+        }
+        gs->unk42E = 0;
+    }
+
+    if (gs->unk427 != 0) {
+        func_80057564_58164(10);
+        n_alSynRemovePlayer(gs);
+        n_alSynRemovePlayer((void *)((u8 *)gs + 0x1D8));
+
+        gs->unk3E4 = func_8006A200_6AE00((void *)gs->unk3E4);
+        gs->unk3DC = func_8006A200_6AE00((void *)gs->unk3DC);
+        gs->unk3E0 = func_8006A200_6AE00((void *)gs->unk3E0);
+
+        if (gs->unk427 == 0xFF) {
+            func_800697CC_6A3CC(func_80017264_17E64);
+        } else {
+            u8 tmp = gs->unk425 + 1;
+            D_800A8CC8_A0038 = tmp;
+            func_800697CC_6A3CC(func_80017248_17E48);
+        }
+    }
+}
 
 void func_80017248_17E48(void) {
     func_800697F4_6A3F4(1);
--- src/gamestate.h
+++ src/gamestate.h
@@ -1,13 +1,21 @@
 #include "common.h"
 
 typedef struct {
-    char padding[0x3F4];
+    char padding[0x3DC];
+    void* unk3DC;
+    void* unk3E0;
+    void* unk3E4;
+    char padding5[0xC];
     s16 unk3F4;
     s32 unk3F8;
     u16 unk3FC;
     char padding2[0x26];
     u8 unk424;
     s8 unk425;
-    char paddding3[3];
+    char padding4[1];
+    u8 unk427;
+    char paddding3[1];
     s8 unk429;
+    char padding3[4];
+    s8 unk42E;
 } GameState;
