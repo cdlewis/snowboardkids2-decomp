1f3de6e8622a6569aaf58430be9980bd6feddb91
Chris Lewis <me@chrislewis.au>
2025-03-29 07:54:52 -0400

    match func_8006A220_6AE20

--- src/6AE20.c
+++ src/6AE20.c
@@ -1,6 +1,21 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/nonmatchings/6AE20", func_8006A220_6AE20);
+extern s32 D_800AB130_A24A0;
+extern s8 D_8018F800;
+
+void func_8006A220_6AE20(void) {
+    s32 var_v1;
+    s8* var_a0;
+
+    D_800AB130_A24A0 = 0;
+    var_a0 = &D_8018F800;
+    var_v1 = 0;
+    do {
+        *var_a0 = 0;
+        var_v1 += 1;
+        var_a0 += 1;
+    } while (var_v1 <= 0x1FFFFF);
+}
 
 INCLUDE_ASM("asm/nonmatchings/6AE20", func_8006A258_6AE58);
 
