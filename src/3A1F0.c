d21b8e7a87d8d261e19ce1a5ec3a5185207d87f6
Chris Lewis <me@chrislewis.au>
2025-03-30 19:06:48 -0400

    match func_8003B510_3C110

--- src/3A1F0.c
+++ src/3A1F0.c
@@ -286,7 +286,18 @@ void func_8003B47C_3C07C(void* arg0) {
     }
 }
 
-INCLUDE_ASM("asm/nonmatchings/3A1F0", func_8003B510_3C110);
+void* func_8003B510_3C110(void) {
+    void* sp10;
+    void* var_v0;
+
+    sp10 = NULL;
+    var_v0 = (void*)-1;
+    if (osRecvMesg(&D_800A1888_A2488, &sp10, 0) == 0) {
+        D_8008FE8F_90A8F = 0;
+        var_v0 = sp10;
+    }
+    return var_v0;
+}
 
 void func_8003B560_3C160(u8* arg0) {
     osSendMesg(
