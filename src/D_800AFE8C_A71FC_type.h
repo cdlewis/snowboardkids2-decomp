9971d19665f79f175c734320b9b1e30c660b638e
Chris Lewis <me@chrislewis.au>
2025-04-01 08:39:30 -0400

    match func_8001BC78_1C878

---
+++ src/D_800AFE8C_A71FC_type.h
@@ -0,0 +1,10 @@
+#include "common.h"
+
+typedef struct {
+    s32 padding;
+    u8 unk4;
+    char padding2[4];
+    u8 unk9;
+} D_800AFE8C_A71FC_type;
+
+extern D_800AFE8C_A71FC_type* D_800AFE8C_A71FC;
