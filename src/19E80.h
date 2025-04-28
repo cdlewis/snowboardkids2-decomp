e9edf88c90a2b6392bf4c46914bb5dd7d0447ec5
Chris Lewis <me@chrislewis.au>
2025-04-27 22:09:43 -0400

    match func_80002C14_3814

---
+++ src/19E80.h
@@ -0,0 +1,10 @@
+#include "common.h"
+
+typedef struct {
+    s32 unk0;
+    s32 unk4;
+    s16 unk18;
+    s16 unk1A;
+} func_80019280_19E80_return;
+
+void func_80019280_19E80(void *, u16, func_80019280_19E80_return *);
