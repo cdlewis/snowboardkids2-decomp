135b790cb628f2b7d39e65a8b1453de75d44902d
Chris Lewis <me@chrislewis.au>
2025-06-19 14:48:27 -0400

    match func_800BB2B0_B74F0

---
+++ src/56910.c
@@ -0,0 +1,35 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/56910", func_80055D10_56910);
+
+INCLUDE_ASM("asm/nonmatchings/56910", func_80055D34_56934);
+
+INCLUDE_ASM("asm/nonmatchings/56910", func_80055D7C_5697C);
+
+INCLUDE_ASM("asm/nonmatchings/56910", func_80055DC4_569C4);
+
+INCLUDE_ASM("asm/nonmatchings/56910", func_80055DF8_569F8);
+
+INCLUDE_ASM("asm/nonmatchings/56910", func_80055E40_56A40);
+
+INCLUDE_ASM("asm/nonmatchings/56910", func_80055E54_56A54);
+
+extern void* D_80091590_92190[];
+
+void* func_80055E68_56A68(s32 index) {
+    return D_80091590_92190[index];
+}
+
+INCLUDE_ASM("asm/nonmatchings/56910", func_80055E7C_56A7C);
+
+INCLUDE_ASM("asm/nonmatchings/56910", func_80055EA4_56AA4);
+
+INCLUDE_ASM("asm/nonmatchings/56910", func_80055ED0_56AD0);
+
+INCLUDE_ASM("asm/nonmatchings/56910", func_80055EFC_56AFC);
+
+INCLUDE_ASM("asm/nonmatchings/56910", func_80055F28_56B28);
+
+INCLUDE_ASM("asm/nonmatchings/56910", func_80055F54_56B54);
+
+INCLUDE_ASM("asm/nonmatchings/56910", func_80055F80_56B80);
--- src/B74F0.c
+++ src/B74F0.c
@@ -1,4 +1,6 @@
+#include "69EF0.h"
 #include "common.h"
+#include "gamestate.h"
 #include "geometry.h"
 
 typedef struct {
@@ -18,11 +20,86 @@ typedef struct {
     s32 unk28;
 } func_800BBA60_B7CA0_arg;
 
-void func_80063788_64388(s32, void *);
-extern void func_8006B084_6BC84(void *, void *, void *);
-extern s32 D_8009A8A4_9B4A4;
+typedef struct {
+    u8 padding[0x90];
+    s32 unk90;
+    s32 padding2[3];
+    s32 unkA0;
+} func_80055E68_56A68_result;
 
-INCLUDE_ASM("asm/nonmatchings/B74F0", func_800BB2B0_B74F0);
+typedef struct {
+    u16 unk0;
+    u16 unk2;
+    s32 unk4[3];
+} D_800BBB90_Entry;
+
+extern D_800BBB90_Entry D_800BBB90_B7DD0[];
+
+typedef struct
+{
+    s8 pad_0[0x20];
+    void *unk20;
+    void *unk24;
+    void *unk28;
+    void *unk2C;
+    s8 pad_30[0x2C];
+    void *unk5C;
+    void *unk60;
+    void *unk64;
+    void *unk68;
+    s8 pad_6C[0xC];
+    s16 unk78;
+    s8 pad_7A[0x12];
+    s16 unk8C[6];
+    s32 unk98;
+    s32 unk9C;
+    s32 unkA0;
+    s16 unkA4;
+    s16 unkA6;
+} func_800BB2B0_B74F0_arg;
+
+extern func_80055E68_56A68_result *func_80055E68_56A68(u8);
+extern s32 func_800AFF44_9FE04(void);
+extern s32 D_8009A8A4_9B4A4;
+extern void *func_80055DC4_569C4(u8);
+extern void *func_80055DF8_569F8(u8);
+extern void func_8006A724_6B324(void *, u16, u16);
+extern void func_8006B084_6BC84(void *, void *, void *);
+extern void func_800BB468_B76A8(void);
+
+void func_80063788_64388(s32, void *);
+void func_800BB7D0_B7A10(func_800BBA60_B7CA0_arg *);
+
+void func_800BB2B0_B74F0(func_800BB2B0_B74F0_arg *arg0) {
+    GameState *gameState;
+    s32 temp;
+    u16 var_v1;
+    func_80055E68_56A68_result *result;
+
+    gameState = GameStateGet();
+    temp = func_800AFF44_9FE04();
+
+    arg0->unkA6 += (temp & 1);
+    result = func_80055E68_56A68(gameState->unk5C);
+    arg0->unk20 = &result->unk90;
+    result = func_80055E68_56A68(gameState->unk5C);
+    arg0->unk5C = &result->unkA0;
+    arg0->unk24 = func_80055DC4_569C4(gameState->unk5C);
+    arg0->unk28 = func_80055DF8_569F8(gameState->unk5C);
+    arg0->unk2C = 0;
+    arg0->unk98 = 0;
+    arg0->unk9C = 0;
+    arg0->unkA0 = 0;
+    arg0->unk60 = arg0->unk24;
+    arg0->unk64 = arg0->unk28;
+    arg0->unk68 = arg0->unk2C;
+    memcpy(&arg0->unk8C, &D_800BBB90_B7DD0[arg0->unkA6].unk4, 0xC);
+    func_8006A724_6B324(&arg0->unk78, D_800BBB90_B7DD0[arg0->unkA6].unk0, D_800BBB90_B7DD0[arg0->unkA6].unk2);
+    arg0->unkA4 = 0;
+
+    setCleanupCallback((void (*)(void *))func_800BB7D0_B7A10);
+    setCallback((void (*)(void *))func_800BB468_B76A8);
+}
 
 void func_800BB3B8_B75F8(func_800BB3B8_B75F8_arg *arg0) {
     s32 matrix[8];  // this should be matrix[9] but it causes stack issues
