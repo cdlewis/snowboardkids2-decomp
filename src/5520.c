f24c9771f2f8e8775b5d1c2e6f1f657c1dd33ef4
Chris Lewis <me@chrislewis.au>
2025-07-26 08:26:46 -0400

    match func_8000576C_636C

--- src/5520.c
+++ src/5520.c
@@ -2,6 +2,21 @@
 #include "common.h"
 #include "displaylist.h"
 
+typedef struct {
+    u8 padding[0x20];
+    struct {
+        s32 unk0;
+        s32 unk4;
+        s32 unk8;
+        s32 unkC;
+    } *unk20;
+    u8 padding2[0xC];
+    s32 unk30;
+} func_8000576C_636C_arg;
+
+void func_800056E8_62E8(DisplayListObject *arg0);
+void func_80005714_6314(s32 arg0);
+void func_80005740_6340(void);
 void func_80004920_5520();
 void func_80005094_5C94();
 void func_800634E8_640E8(s32);
@@ -64,7 +79,18 @@ void func_80005714_6314(s32 arg0) {
 
 INCLUDE_ASM("asm/nonmatchings/5520", func_80005740_6340);
 
-INCLUDE_ASM("asm/nonmatchings/5520", func_8000576C_636C);
+void func_8000576C_636C(s32 arg0, func_8000576C_636C_arg *arg1) {
+    arg1->unk30 = 0;
+    if (arg1->unk20->unk4 != 0) {
+        debugEnqueueCallback(arg0 & 0xFFFF, 1, &func_800056E8_62E8, arg1);
+    }
+    if (arg1->unk20->unk8 != 0) {
+        debugEnqueueCallback(arg0 & 0xFFFF, 3, &func_80005714_6314, arg1);
+    }
+    if (arg1->unk20->unkC != 0) {
+        debugEnqueueCallback(arg0 & 0xFFFF, 5, &func_80005740_6340, arg1);
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/5520", func_80005808_6408);
 
