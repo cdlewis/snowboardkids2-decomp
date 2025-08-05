bf44493d6fd58ab86b2467521b4d5901ee12d96f
Chris Lewis <me@chrislewis.au>
2025-08-05 18:32:54 -0400

    add unused triggers

---
+++ src/unused_triggers.c
@@ -0,0 +1,12 @@
+#include "common.h"
+
+char PaintShopLabel[20] = "PAINT SHOP";
+char InformationLabel[20] = "INFORMATION ";
+char ShootCrossLabel[20] = "SHOOT CROSS";
+char ChrSelectLabel[20] = "CHR SELECT";
+char BoardShopLabel[20] = "BOARD SHOP";
+char TrickGameLabel[20] = "TRICK GAME";
+char SaveLoadLabel[20] = "SAVE LOAD";
+char RankingLabel[20] = "RANKING";
+char SpeedCrossLabel[20] = "SPEED CROSS";
+char SendenLabel[20] = "SENDEN";
