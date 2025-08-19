000450192d468dae8f449de4e5e92440123f6a52
Chris Lewis <me@chrislewis.au>
2025-08-18 22:42:30 -0400

    match func_8000CCBC_D8BC

--- src/D6F0.c
+++ src/D6F0.c
@@ -66,6 +66,26 @@ typedef struct {
     void *unk4;
 } func_8000CC84_D884_arg;
 
+typedef struct {
+    u8 padding[0xC];
+    s32 unkC;
+    u8 padding2[0x1C];
+    s32 unk2C;
+    u8 padding3[0x18];
+    s32 unk48;
+    s32 unk4C;
+
+    s32 unk50;
+    s16 unk54;
+    s16 unk56;
+} func_8000CAF0_D6F0_task_memory;
+
+typedef struct {
+    s32 unk0;
+    s32 unk4;
+    s32 unk8;
+} func_8000CCBC_D8BC_arg2;
+
 void func_8000CB94_D794(func_8000CB94_D794_arg *arg0);
 void func_8000CC84_D884(func_8000CC84_D884_arg *);
 void func_80067EDC_68ADC(u16, void *);
@@ -117,7 +137,18 @@ void func_8000CC84_D884(func_8000CC84_D884_arg *arg0) {
     arg0->unk0 = freeNodeMemory(arg0->unk0);
 }
 
-INCLUDE_ASM("asm/nonmatchings/D6F0", func_8000CCBC_D8BC);
+void func_8000CCBC_D8BC(void *arg0, void *arg1, func_8000CCBC_D8BC_arg2 *arg2, s32 arg3) {
+    func_8000CAF0_D6F0_task_memory *temp_v0 =
+        (func_8000CAF0_D6F0_task_memory *)scheduleTask(&func_8000CAF0_D6F0, 0, 0, 0);
+    if (temp_v0 != NULL) {
+        memcpy(&temp_v0->unkC, arg0, 0xC);
+        memcpy(&temp_v0->unk2C, arg1, 0xC);
+        temp_v0->unk56 = arg3;
+        temp_v0->unk48 = (s32)((s32)arg2->unk0 / 2);
+        temp_v0->unk4C = (s32)((s32)arg2->unk4 / 2);
+        temp_v0->unk50 = (s32)((s32)arg2->unk8 / 2);
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/D6F0", func_8000CD88_D988);
 
