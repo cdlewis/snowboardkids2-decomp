4bb9a766c2682d2f13cc94c04faae5d8fb8d8e5d
Chris Lewis <me@chrislewis.au>
2025-09-06 17:00:11 -0400

    start to match 1DED40

---
+++ src/1DED40.c
@@ -0,0 +1,57 @@
+#include "1DED40.h"
+
+void noop_1DED40(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/1DED40", func_800B1C98_1DED48);
+
+INCLUDE_ASM("asm/nonmatchings/1DED40", func_800B1CA0_1DED50);
+
+INCLUDE_ASM("asm/nonmatchings/1DED40", func_800B1D44_1DEDF4);
+
+void noop_1DEDFC(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/1DED40", func_800B1D54_1DEE04);
+
+INCLUDE_ASM("asm/nonmatchings/1DED40", func_800B1D5C_1DEE0C);
+
+INCLUDE_ASM("asm/nonmatchings/1DED40", func_800B1D9C_1DEE4C);
+
+void noop_1DEE54(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/1DED40", func_800B1DAC_1DEE5C);
+
+INCLUDE_ASM("asm/nonmatchings/1DED40", func_800B1DB4_1DEE64);
+
+INCLUDE_ASM("asm/nonmatchings/1DED40", func_800B1DF4_1DEEA4);
+
+void noop_1DEEAC(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/1DED40", func_800B1E04_1DEEB4);
+
+INCLUDE_ASM("asm/nonmatchings/1DED40", func_800B1E0C_1DEEBC);
+
+INCLUDE_ASM("asm/nonmatchings/1DED40", func_800B1F94_1DF044);
+
+void noop_1DF04C(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/1DED40", func_800B1FA4_1DF054);
+
+INCLUDE_ASM("asm/nonmatchings/1DED40", func_800B1FAC_1DF05C);
+
+INCLUDE_ASM("asm/nonmatchings/1DED40", func_800B2010_1DF0C0);
+
+INCLUDE_ASM("asm/nonmatchings/1DED40", func_800B20A8_1DF158);
+
+void noop_1DF160(void) {
+}
+
+s32 func_800B20B8_1DF168(void) {
+    return 0;
+}
+
+INCLUDE_ASM("asm/nonmatchings/1DED40", func_800B20C0_1DF170);
