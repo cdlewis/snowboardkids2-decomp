25cf5f998e0549fc3965ee5d46cc9d7be62db715
Chris Lewis <me@chrislewis.au>
2025-09-06 17:39:49 -0400

    start to match 1DF180

---
+++ src/1DF180.c
@@ -0,0 +1,24 @@
+#include "1DF180.h"
+
+INCLUDE_ASM("asm/nonmatchings/1DF180", func_800B20D0_1DF180);
+
+void noop_1DF1C8(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/1DF180", func_800B2120_1DF1D0);
+
+INCLUDE_ASM("asm/nonmatchings/1DF180", func_800B2128_1DF1D8);
+
+void noop_1DF23C(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/1DF180", func_800B2194_1DF244);
+
+INCLUDE_ASM("asm/nonmatchings/1DF180", func_800B219C_1DF24C);
+
+void noop_1DF2D0(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/1DF180", func_800B2228_1DF2D8);
+
+INCLUDE_ASM("asm/nonmatchings/1DF180", func_800B2230_1DF2E0);
