65dcc73f06661e26e6e7d5d308c066e7ecf862a0
Chris Lewis <me@chrislewis.au>
2025-09-06 17:37:01 -0400

    start to match 1DD970

---
+++ src/1DD970.c
@@ -0,0 +1,33 @@
+#include "1DD970.h"
+
+void noop_1DD970(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/1DD970", func_800B08C8_1DD978);
+
+INCLUDE_ASM("asm/nonmatchings/1DD970", func_800B08D0_1DD980);
+
+void noop_1DD9C0(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/1DD970", func_800B0918_1DD9C8);
+
+INCLUDE_ASM("asm/nonmatchings/1DD970", func_800B0920_1DD9D0);
+
+INCLUDE_ASM("asm/nonmatchings/1DD970", func_800B099C_1DDA4C);
+
+void noop_1DDA54(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/1DD970", func_800B09AC_1DDA5C);
+
+INCLUDE_ASM("asm/nonmatchings/1DD970", func_800B09B4_1DDA64);
+
+void noop_1DDAA4(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/1DD970", func_800B09FC_1DDAAC);
+
+INCLUDE_ASM("asm/nonmatchings/1DD970", func_800B0A04_1DDAB4);
+
+INCLUDE_ASM("asm/nonmatchings/1DD970", func_800B0A2C_1DDADC);
--- src/1DD970.h
+++ src/1DD970.h
@@ -1,14 +1,16 @@
-void func_800B08C0_1DD970(void);
+#include "common.h"
+
+void noop_1DD970(void);
 void func_800B08C8_1DD978(void);
 void func_800B08D0_1DD980(void);
-void func_800B0910_1DD9C0(void);
+void noop_1DD9C0(void);
 void func_800B0918_1DD9C8(void);
 void func_800B0920_1DD9D0(void);
 void func_800B099C_1DDA4C(void);
-void func_800B09A4_1DDA54(void);
+void noop_1DDA54(void);
 void func_800B09AC_1DDA5C(void);
 void func_800B09B4_1DDA64(void);
-void func_800B09F4_1DDAA4(void);
+void noop_1DDAA4(void);
 void func_800B09FC_1DDAAC(void);
 void func_800B0A04_1DDAB4(void);
 void func_800B0A2C_1DDADC(void);
