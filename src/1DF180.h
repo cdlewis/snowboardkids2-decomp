a44a006880feb56a2ed6380f8039ebd828ea4f98
Chris Lewis <me@chrislewis.au>
2025-09-06 12:18:53 -0400

    improve 1E6D20 data match

---
+++ src/1DF180.h
@@ -0,0 +1,9 @@
+void func_800B2118_1DF1C8(void);
+void func_800B2120_1DF1D0(void);
+void func_800B2128_1DF1D8(void);
+void func_800B218C_1DF23C(void);
+void func_800B2194_1DF244(void);
+void func_800B219C_1DF24C(void);
+void func_800B2220_1DF2D0(void);
+void func_800B2228_1DF2D8(void);
+void func_800B2230_1DF2E0(void);
---
+++ src/1DF8B0.h
@@ -0,0 +1,7 @@
+void func_800B2800_1DF8B0(void);
+void func_800B2808_1DF8B8(void);
+void func_800B2810_1DF8C0(void);
+void func_800B292C_1DF9DC(void);
+void func_800B2934_1DF9E4(void);
+void func_800B293C_1DF9EC(void);
+void func_800B2944_1DF9F4(void);
