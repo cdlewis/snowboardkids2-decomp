ff68d4ca2cfd5213bbc8973013f83c4f65d610b1
Chris Lewis <me@chrislewis.au>
2025-09-15 20:42:14 -0400

    match func_800B0920_1DD9D0

--- src/1DD970.c
+++ src/1DD970.c
@@ -1,5 +1,12 @@
 #include "1DD970.h"
 
+extern void func_800B5F2C_1E2FDC(s32, s16, s16);
+extern void func_800B5FB0_1E3060(s32, s16, s16);
+extern void func_800B6034_1E30E4(s32, s32, s16);
+extern void func_800B6088_1E3138(s32, s32, s16);
+extern void func_800B60DC_1E318C(s32, s32, s16);
+extern void func_800B5EC4_1E2F74(s32, s16, s16, s32, s32, s32);
+
 void noop_1DD970(void) {
 }
 
@@ -7,7 +14,9 @@ s32 returnZero_1DD978(void) {
     return 0;
 }
 
-INCLUDE_ASM("asm/nonmatchings/1DD970", func_800B08D0_1DD980);
+void func_800B08D0_1DD980(func_800B08D0_1DD980_arg0 *arg0, func_800B08D0_1DD980_arg1 *arg1) {
+    func_800B5EC4_1E2F74(arg1->unkC, arg0->unk0, arg0->unk2, arg0->unk4, arg0->unk8, arg0->unkC);
+}
 
 void noop_1DD9C0(void) {
 }
@@ -16,7 +25,13 @@ s32 returnZero_1DD9C8(void) {
     return 0;
 }
 
-INCLUDE_ASM("asm/nonmatchings/1DD970", func_800B0920_1DD9D0);
+void func_800B0920_1DD9D0(func_800B0920_1DD9D0_arg0 *arg0, func_800B0920_1DD9D0_arg1 *arg1) {
+    func_800B5F2C_1E2FDC(arg1->unkC, arg0->unk0, arg0->unkC);
+    func_800B5FB0_1E3060(arg1->unkC, arg0->unk2, arg0->unkC);
+    func_800B6034_1E30E4(arg1->unkC, arg0->unk10, arg0->unkC);
+    func_800B6088_1E3138(arg1->unkC, arg0->unk4, arg0->unkC);
+    func_800B60DC_1E318C(arg1->unkC, arg0->unk8, arg0->unkC);
+}
 
 s32 returnZero_1DDA4C(void) {
     return 0;
--- src/1DD970.h
+++ src/1DD970.h
@@ -9,12 +9,36 @@ typedef struct {
     s32 unkC;
 } func_800B0A04_1DDAB4_arg1;
 
+typedef struct {
+    s16 unk0;
+    s16 unk2;
+    s32 unk4;
+    s32 unk8;
+    s32 unkC;
+} func_800B08D0_1DD980_arg0;
+typedef struct {
+    u8 padding[0xC];
+    s32 unkC;
+} func_800B08D0_1DD980_arg1;
+typedef struct {
+    s16 unk0;
+    s16 unk2;
+    s32 unk4;
+    s32 unk8;
+    s16 unkC;
+    s32 unk10;
+} func_800B0920_1DD9D0_arg0;
+typedef struct {
+    u8 padding[0xC];
+    s32 unkC;
+} func_800B0920_1DD9D0_arg1;
+
 void noop_1DD970(void);
 s32 returnZero_1DD978(void);
-void func_800B08D0_1DD980(void);
+void func_800B08D0_1DD980(func_800B08D0_1DD980_arg0 *arg0, func_800B08D0_1DD980_arg1 *arg1);
 void noop_1DD9C0(void);
 s32 returnZero_1DD9C8(void);
-void func_800B0920_1DD9D0(void);
+void func_800B0920_1DD9D0(func_800B0920_1DD9D0_arg0 *arg0, func_800B0920_1DD9D0_arg1 *arg1);
 s32 returnZero_1DDA4C(void);
 void noop_1DDA54(void);
 s32 returnZero_1DDA5C(void);
