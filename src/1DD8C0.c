e975ba3f2d92f4771eb068d31b5f90c24df5304d
Chris Lewis <me@chrislewis.au>
2025-09-16 07:38:04 -0400

    match 1DD8C0

--- src/1DD8C0.c
+++ src/1DD8C0.c
@@ -1,5 +1,9 @@
 #include "1DD8C0.h"
 
+void func_80057514_58114(s16, u16, u16);
+void func_80057564_58164(s16);
+void func_80057550_58150(u16, u16);
+
 void noop_1DD8C0(void) {
 }
 
@@ -7,7 +11,9 @@ s32 returnZero_1DD8C8(void) {
     return 0;
 }
 
-INCLUDE_ASM("asm/nonmatchings/1DD8C0", func_800B0820_1DD8D0);
+void func_800B0820_1DD8D0(func_800B0820_1DD8D0_arg0 *arg0) {
+    func_80057514_58114(arg0->unk0, arg0->unk4, arg0->unk2);
+}
 
 void noop_1DD8F8(void) {
 }
@@ -16,7 +22,13 @@ s32 returnZero_1DD900(void) {
     return 0;
 }
 
-INCLUDE_ASM("asm/nonmatchings/1DD8C0", func_800B0858_1DD908);
+void func_800B0858_1DD908(s16 *arg0) {
+    if (*arg0 < 4) {
+        func_80057564_58164(4);
+    } else {
+        func_80057564_58164(*arg0);
+    }
+}
 
 void noop_1DD934(void) {
 }
@@ -25,4 +37,6 @@ s32 returnZero_1DD93C(void) {
     return 0;
 }
 
-INCLUDE_ASM("asm/nonmatchings/1DD8C0", func_800B0894_1DD944);
+void func_800B0894_1DD944(func_800B0820_1DD8D0_arg0 *arg0) {
+    func_80057550_58150(arg0->unk2, arg0->unk0);
+}
--- src/1DD8C0.h
+++ src/1DD8C0.h
@@ -1,11 +1,17 @@
 #include "common.h"
 
+typedef struct {
+    s16 unk0;
+    s16 unk2;
+    s16 unk4;
+} func_800B0820_1DD8D0_arg0;
+
 void noop_1DD8C0(void);
 s32 returnZero_1DD8C8(void);
-void func_800B0820_1DD8D0(void);
+void func_800B0820_1DD8D0(func_800B0820_1DD8D0_arg0 *arg0);
 void noop_1DD8F8(void);
 s32 returnZero_1DD900(void);
-void func_800B0858_1DD908(void);
+void func_800B0858_1DD908(s16 *arg0);
 void noop_1DD934(void);
 s32 returnZero_1DD93C(void);
-void func_800B0894_1DD944(void);
+void func_800B0894_1DD944(func_800B0820_1DD8D0_arg0 *arg0);
