60af3e276d01a5e4ba0678983a0acad94ea7cee6
Chris Lewis <me@chrislewis.au>
2025-09-23 21:48:20 -0400

    match func_800B2944_1DF9F4

--- src/1DF8B0.c
+++ src/1DF8B0.c
@@ -1,4 +1,9 @@
 #include "1DF8B0.h"
+#include "task_scheduler.h"
+
+void func_800B29F0_1DFAA0(void *);
+
+extern void func_800B993C_1E69EC(void);
 
 void noop_1DF8B(void) {
 }
@@ -20,4 +25,21 @@ s32 returnZero_1DF9EC(void) {
     return 0;
 }
 
-INCLUDE_ASM("asm/nonmatchings/1DF8B0", func_800B2944_1DF9F4);
+void func_800B2944_1DF9F4(func_800B2944_1DF9F4_arg0 *arg0, u8 *arg1, s8 arg2) {
+    func_800B2944_1DF9F4_asset *temp_v0;
+    s32 *ptr;
+
+    ptr = (s32 *)(arg1 + (arg2 * 244 + 0xA8));
+    func_800B29F0_1DFAA0(arg1);
+
+    temp_v0 = (func_800B2944_1DF9F4_asset *)scheduleTask(&func_800B993C_1E69EC, 1, 0, 0x64);
+    if (temp_v0) {
+        temp_v0->unk0 = arg1;
+        temp_v0->unk4 = ptr[0x48 / 4];
+        temp_v0->unk10 = arg0->unk2;
+        temp_v0->unk12 = arg0->unk4;
+        temp_v0->unk86 = arg0->unk0;
+    } else {
+        arg1[0xFF7] = 0;
+    }
+}
--- src/1DF8B0.h
+++ src/1DF8B0.h
@@ -1,9 +1,25 @@
 #include "common.h"
 
+typedef struct {
+    u16 unk0;
+    u16 unk2;
+    u16 unk4;
+} func_800B2944_1DF9F4_arg0;
+
+typedef struct {
+    void *unk0;
+    s32 unk4;
+    u8 padding[0x8];
+    s16 unk10;
+    s16 unk12;
+    u8 padding2[0x72];
+    s16 unk86;
+} func_800B2944_1DF9F4_asset;
+
 void noop_1DF8B(void);
 s32 returnZero_1DF8B8(void);
 void func_800B2810_1DF8C0(void);
 s32 returnZero_1DF9DC(void);
 void noop_1DF9E4(void);
 s32 returnZero_1DF9EC(void);
-void func_800B2944_1DF9F4(void);
+void func_800B2944_1DF9F4(func_800B2944_1DF9F4_arg0 *arg0, u8 *arg1, s8 arg2);
