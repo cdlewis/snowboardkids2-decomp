1e853e79b5f5972b4cde2227909d24c0bd9a39b2
Chris Lewis <me@chrislewis.au>
2025-10-26 08:04:59 -0400

    match func_80006E44_7A44

--- src/7850.c
+++ src/7850.c
@@ -21,8 +21,9 @@ void func_80007030_7C30(func_80007030_7C30_arg *);
 void func_80009E68_AA68(s32 *, s32);
 void func_80009F90_AB90(s32 *, s32, s32, s32);
 
+extern void func_80009F5C_AB5C(s32 *);
 extern void func_80006CBC_78BC(void);
-extern void func_80006E44_7A44(void);
+extern void func_80006E44_7A44(func_80006C50_7850_arg *);
 extern void *D_80088720_89320;
 extern void *func_80006EE0_7AE0(void);
 extern u8 identityMatrix[];
@@ -41,7 +42,9 @@ void func_80006C50_7850(func_80006C50_7850_arg *arg0) {
 
 INCLUDE_ASM("asm/nonmatchings/7850", func_80006CBC_78BC);
 
-INCLUDE_ASM("asm/nonmatchings/7850", func_80006E44_7A44);
+void func_80006E44_7A44(func_80006C50_7850_arg *arg0) {
+    func_80009F5C_AB5C(&arg0->unk4);
+}
 
 typedef struct {
     void *unk0;
