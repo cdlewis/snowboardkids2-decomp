5593bd30100a59b234ec1014d8e75ceeadab2de9
Chris Lewis <me@chrislewis.au>
2025-11-09 20:37:24 -0500

    match func_800B2374_1DF424

--- src/1DFAA0.h
+++ src/1DFAA0.h
@@ -2,10 +2,20 @@
 
 #include "common.h"
 
+typedef union {
+    struct {
+        s16 lower;
+        s16 upper;
+    } half;
+    s32 full;
+} CompositeInt;
+
 typedef struct {
-    s32 unk0;
-    s32 unk4;
-    u8 padding[0x38];
+    CompositeInt unk0;
+    CompositeInt unk4;
+    u8 padding5[0x2];
+    u8 unkA;
+    u8 padding[0x35];
     s8 unk40;
     u8 padding2[0x3];
     s8 unk44;
@@ -35,3 +45,5 @@ void func_800B29F0_1DFAA0(func_800B29F0_1DFAA0_arg *arg0);
 void func_800B2A50_1DFB00(func_800B2A24_1DFAD4_arg *arg0, s16 arg1);
 
 func_800B2A24_1DFAD4_arg_item *func_800B2A78_1DFB28(func_800B2A24_1DFAD4_arg *arg0, s16 arg1);
+
+void func_800B2A24_1DFAD4(func_800B2A24_1DFAD4_arg *arg0, s16 arg1);
