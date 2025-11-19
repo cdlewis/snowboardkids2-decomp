af0a68b636485789a6312e9ba3303aa6566f611c
Chris Lewis <me@chrislewis.au>
2025-11-18 19:02:11 -0500

    match func_800B3420_1E04D0

--- src/cutscene/1DFAA0.c
+++ src/cutscene/1DFAA0.c
@@ -212,7 +212,9 @@ s32 processCutsceneFrame(CutsceneManager *uiManager) {
 
 INCLUDE_ASM("asm/nonmatchings/cutscene/1DFAA0", func_800B3360);
 
-INCLUDE_ASM("asm/nonmatchings/cutscene/1DFAA0", func_800B3420_1E04D0);
+void *func_800B3420_1E04D0(void) {
+    return &D_800BAEBC_1E7F6C->padding0[4];
+}
 
 void *func_800B3430_1E04E0(void) {
     return &D_800BAEBC_1E7F6C->padding0[8];
