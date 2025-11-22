e0f9c20a65c4cd14394d45b2cc96717a56b3630d
Chris Lewis <me@chrislewis.au>
2025-11-21 21:59:03 -0500

    matched func_800B4C80_1E1D30

--- src/1E1BA0.c
+++ src/1E1BA0.c
@@ -29,7 +29,14 @@ INCLUDE_ASM("asm/nonmatchings/1E1BA0", func_800B4B30_1E1BE0);
 
 INCLUDE_ASM("asm/nonmatchings/1E1BA0", func_800B4BDC_1E1C8C);
 
-INCLUDE_ASM("asm/nonmatchings/1E1BA0", func_800B4C80_1E1D30);
+extern void func_80058220_58E20(s32, s32);
+
+void func_800B4C80_1E1D30(s16 arg0) {
+    s16 temp;
+
+    temp = func_800B4AFC_1E1BAC(arg0);
+    func_80058220_58E20(temp, 0);
+}
 
 void func_800B4CB0_1E1D60(void) {
     func_80057ABC_586BC(0, 0);
