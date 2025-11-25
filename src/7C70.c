498ce1fec6d83adf6a7da5f2b4c6c6a36dc40a56
Chris Lewis <me@chrislewis.au>
2025-11-25 09:12:10 -0500

    matched func_80007360_7F60 1 attempt

--- src/7C70.c
+++ src/7C70.c
@@ -19,4 +19,6 @@ INCLUDE_ASM("asm/nonmatchings/7C70", func_8000714C_7D4C);
 
 INCLUDE_ASM("asm/nonmatchings/7C70", func_800071E4_7DE4);
 
-INCLUDE_ASM("asm/nonmatchings/7C70", func_80007360_7F60);
+void func_80007360_7F60(func_80007130_7D30_arg *arg0) {
+    func_80009F5C_AB5C(&arg0->unk4);
+}
