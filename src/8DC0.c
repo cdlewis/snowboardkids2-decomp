dde5c872209b2a945aca4382c3efdbbe40dc4cee
Chris Lewis <me@chrislewis.au>
2025-11-26 20:46:30 -0500

    matched func_80008310_8F10 1 attempt

--- src/8DC0.c
+++ src/8DC0.c
@@ -2,9 +2,13 @@
 
 extern void setCleanupCallback(void *);
 extern void setCallback(void *);
+extern void func_80009E68_AA68(void *, s32);
+extern void func_80009F90_AB90(void *, s32, s32, s32);
 
 void func_800081EC_8DEC(void);
 void func_80008308_8F08(void);
+void func_80008370_8F70(void);
+void func_800084A8_90A8(void);
 
 void func_800081C0_8DC0(void) {
     setCleanupCallback(func_80008308_8F08);
@@ -16,7 +20,19 @@ INCLUDE_ASM("asm/nonmatchings/8DC0", func_800081EC_8DEC);
 void func_80008308_8F08(void) {
 }
 
-INCLUDE_ASM("asm/nonmatchings/8DC0", func_80008310_8F10);
+typedef struct {
+    u8 unk0[0x4];
+    u8 unk4[0x4C];
+    u8 unk50;
+} Arg8F10;
+
+void func_80008310_8F10(Arg8F10 *arg0) {
+    arg0->unk50 = 0;
+    func_80009E68_AA68(&arg0->unk4, 9);
+    func_80009F90_AB90(&arg0->unk4, 0x10000, 0, -1);
+    setCleanupCallback(func_800084A8_90A8);
+    setCallback(func_80008370_8F70);
+}
 
 INCLUDE_ASM("asm/nonmatchings/8DC0", func_80008370_8F70);
 
