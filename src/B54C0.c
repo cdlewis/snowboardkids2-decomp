463704a3f1c3907220a8c638e32e91a9fbb4c6a4
Chris Lewis <me@chrislewis.au>
2025-11-28 18:54:22 -0500

    matched func_800BB420_B5630 1 attempts

--- src/B54C0.c
+++ src/B54C0.c
@@ -1,10 +1,20 @@
 #include "common.h"
+#include "task_scheduler.h"
 
 INCLUDE_ASM("asm/nonmatchings/B54C0", func_800BB2B0_B54C0);
 
 INCLUDE_ASM("asm/nonmatchings/B54C0", func_800BB350_B5560);
 
-INCLUDE_ASM("asm/nonmatchings/B54C0", func_800BB420_B5630);
+typedef struct {
+    u8 _pad[0x24];
+    void *unk24;
+    void *unk28;
+} func_800BB420_B5630_arg;
+
+void func_800BB420_B5630(func_800BB420_B5630_arg *arg0) {
+    arg0->unk24 = freeNodeMemory(arg0->unk24);
+    arg0->unk28 = freeNodeMemory(arg0->unk28);
+}
 
 INCLUDE_ASM("asm/nonmatchings/B54C0", func_800BB458_B5668);
 
