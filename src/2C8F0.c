6a901609f379c6c4074214e46c2f00f457cd34ac
Chris Lewis <me@chrislewis.au>
2025-11-27 21:41:30 -0500

    matched func_8002BE44_2CA44 1 attempt

--- src/2C8F0.c
+++ src/2C8F0.c
@@ -1,8 +1,22 @@
 #include "common.h"
+#include "task_scheduler.h"
+
+extern void *D_8008EBF0_8F7F0[];
+
+typedef struct {
+    u8 pad[0xD4];
+    u8 unkD4;
+} func_8002BE44_arg;
 
 INCLUDE_ASM("asm/nonmatchings/2C8F0", func_8002BCF0_2C8F0);
 
-INCLUDE_ASM("asm/nonmatchings/2C8F0", func_8002BE44_2CA44);
+void func_8002BE44_2CA44(func_8002BE44_arg *arg0) {
+    GameState *allocation = (GameState *)getCurrentAllocation();
+
+    if (allocation->unk429 == 0) {
+        setCallback(D_8008EBF0_8F7F0[arg0->unkD4]);
+    }
+}
 
 void func_8002BE8C_2CA8C(void) {
 }
