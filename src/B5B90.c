d839beeae11ee76cf2b5b7dfd10809ed33f1fa6d
Chris Lewis <me@chrislewis.au>
2025-11-29 10:36:44 -0500

    matched func_800BB87C_B615C 14

--- src/B5B90.c
+++ src/B5B90.c
@@ -8,6 +8,7 @@ USE_ASSET(_40E1C0);
 extern void *func_80035F80_36B80(s32);
 extern void func_800BB320_B5C00(void *);
 extern void func_8006D7B0_6E3B0(s32, s16, s16, s16, s16, u8, u8, u8, u8, u8);
+extern void func_800BB928_B6208(void);
 
 typedef struct {
     void *unk0;
@@ -76,7 +77,41 @@ INCLUDE_ASM("asm/nonmatchings/B5B90", func_800BB670_B5F50);
 
 INCLUDE_ASM("asm/nonmatchings/B5B90", func_800BB724_B6004);
 
-INCLUDE_ASM("asm/nonmatchings/B5B90", func_800BB87C_B615C);
+#define GET_UNK16_AS_S16(arg0) (*(s16 *)&(arg0)->unk16)
+#define SET_UNK16_AS_S16(arg0, val) (*(s16 *)&(arg0)->unk16 = (val))
+
+void func_800BB87C_B615C(func_800BB5CC_state *arg0) {
+    s16 temp_v1;
+    s16 temp_a7;
+    s16 temp_a6;
+
+    arg0->unk12--;
+
+    temp_v1 = GET_UNK16_AS_S16(arg0);
+    if (temp_v1 != 0) {
+        SET_UNK16_AS_S16(arg0, temp_v1 - 0x10);
+    }
+
+    if (arg0->unk12 == 1) {
+        setCallback(func_800BB928_B6208);
+    }
+
+    temp_a7 = arg0->unk10;
+    temp_a6 = arg0->unk12;
+
+    func_8006D7B0_6E3B0(
+        (s32)arg0->unk0,
+        ((-temp_a7) << 19) >> 16,
+        ((-temp_a6) << 19) >> 16,
+        temp_a7,
+        temp_a6,
+        0,
+        (u8)arg0->unk14,
+        arg0->unk17,
+        0xC,
+        0x6
+    );
+}
 
 INCLUDE_ASM("asm/nonmatchings/B5B90", func_800BB928_B6208);
 
