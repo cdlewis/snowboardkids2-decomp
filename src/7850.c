c1781fd214270b6a58f9aff84486bd2fc54a9aba
Chris Lewis <me@chrislewis.au>
2025-11-29 16:57:51 -0500

    🧑 matched func_80006CBC_78BC

--- src/7850.c
+++ src/7850.c
@@ -64,7 +64,71 @@ void func_80006C50_7850(func_80006C50_7850_arg *arg0) {
     setCallback(&func_80006CBC_78BC);
 }
 
-INCLUDE_ASM("asm/nonmatchings/7850", func_80006CBC_78BC);
+void func_80006CBC_78BC(func_80006C50_7850_arg *arg0) {
+    s8 sp28[2];
+    func_80006CBC_inner *temp;
+    func_80006CBC_inner *v0;
+    s32 s3;
+    s32 s4;
+    s32 s1;
+
+    do {
+    } while (0);
+
+    sp28[0] = 1;
+    sp28[1] = -1;
+
+    if (arg0->unk0->unk3C == 1) {
+        func_80069CF8_6A8F8();
+        return;
+    }
+
+    switch (arg0->unk0->unk3D) {
+        default:
+
+        case 0:
+            arg0->unk54 += arg0->unk58;
+            if (arg0->unk54 > 0xC80000) {
+                arg0->unk58 = 0xFFF80000;
+            } else if (arg0->unk54 <= 0x63FFFF) {
+                arg0->unk58 = 0x80000;
+            }
+            break;
+
+        case 1:
+            arg0->unk54 += 0xFFF80000;
+            if (arg0->unk54 < 0) {
+                arg0->unk54 = 0;
+            }
+            break;
+
+        case 2:
+            arg0->unk54 += 0x40000;
+            if (arg0->unk54 > 0xC80000) {
+                v0 = arg0->unk0;
+                arg0->unk58 = 0xC80000;
+                v0->unk3D = 0;
+            }
+            break;
+    }
+
+    v0 = arg0->unk0;
+    s3 = v0->unk2C;
+    s4 = v0->unk30;
+    s1 = v0->unk34;
+    func_8000A030_AC30(&arg0->unk4, 0x10000);
+
+    temp = arg0->unk0;
+    if (temp->unk88 == 0) {
+        return;
+    }
+
+    if (temp->unk3F == 0) {
+        return;
+    }
+
+    func_8000A190_AD90(&arg0->unk4, temp->unk10->unk16, s3, s4, s1, 0x10000, 0x10000, 0, 0, ((u8 *)(&arg0->unk54))[1]);
+}
 
 void func_80006E44_7A44(func_80006C50_7850_arg *arg0) {
     func_80009F5C_AB5C(&arg0->unk4);
