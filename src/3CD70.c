329d1959ba7cd82c3e8cd141d78a38a2c1b470a1
Chris Lewis <me@chrislewis.au>
2025-12-03 20:07:54 -0500

    matched func_8003D0F4_3DCF4 10

--- src/3CD70.c
+++ src/3CD70.c
@@ -16,7 +16,8 @@ void func_8003CEC4_3DAC4(u8 arg0) {
 }
 
 void func_8003CF40_3DB40(void);
-void func_8003D0F4_3DCF4(void);
+void func_8003D0F4_3DCF4(NodeExt *arg0);
+void func_8003D210_3DE10(void);
 
 typedef struct {
     s16 unk0;
@@ -34,13 +35,56 @@ void func_8003D0C8_3DCC8(void) {
     scheduleTask(func_8003CF0C_3DB0C, 1, 0, 0xF0);
 }
 
-INCLUDE_ASM("asm/nonmatchings/3CD70", func_8003D0F4_3DCF4);
+extern u8 D_800901CC_90DCC[];
+extern u8 D_800901CE_90DCE[];
+extern u8 D_800901D0_90DD0[];
+extern u8 D_800901D4_90DD4[];
+
+void func_8003D0F4_3DCF4(NodeExt *arg0) {
+    s8 temp_v1;
+    s32 offset;
+
+    temp_v1 = arg0->unk26;
+    offset = temp_v1 * 12;
+    arg0->unk18 = *(void **)(D_800901D0_90DD0 + offset);
+
+    temp_v1 = arg0->unk26;
+    offset = temp_v1 * 12;
+    arg0->unk1C = *(void **)(D_800901D4_90DD4 + offset);
+
+    temp_v1 = arg0->unk26;
+    offset = temp_v1 * 12;
+    arg0->unk24 = *(u16 *)(D_800901CC_90DCC + offset);
+
+    temp_v1 = arg0->unk26;
+    offset = temp_v1 * 12;
+    arg0->unk25 = *(u16 *)(D_800901CE_90DCE + offset);
+
+    memcpy(arg0, (u8 *)arg0->unk18 + 4, 0xC);
+
+    arg0->unk20 = *(u16 *)arg0->unk18;
+    if (arg0->unk20 == 0) {
+        arg0->unk18 = (u8 *)arg0->unk18 + 0x10;
+        arg0->unk20 = *(u16 *)arg0->unk18;
+    }
+
+    if (arg0->unk1C != NULL) {
+        memcpy(&arg0->unkC, (u8 *)arg0->unk1C + 4, 0xC);
+        arg0->unk22 = *(u16 *)arg0->unk1C;
+        if (arg0->unk22 == 0) {
+            arg0->unk1C = (u8 *)arg0->unk1C + 0x10;
+            arg0->unk22 = *(u16 *)arg0->unk1C;
+        }
+    }
+
+    setCallbackWithContinue(func_8003D210_3DE10);
+}
 
 INCLUDE_ASM("asm/nonmatchings/3CD70", func_8003D210_3DE10);
 
 void func_8003D51C_3E11C(u8 arg0) {
-    Node *task = scheduleTask(func_8003D0F4_3DCF4, 0, 2, 0xF0);
+    NodeExt *task = (NodeExt *)scheduleTask(func_8003D0F4_3DCF4, 0, 2, 0xF0);
     if (task != NULL) {
-        *(u8 *)((u8 *)task + 0x26) = arg0;
+        task->unk26 = arg0;
     }
 }
