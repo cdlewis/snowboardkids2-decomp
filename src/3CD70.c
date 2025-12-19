e75759666448ef5fc38ffb19b274b3a8e186538f
Chris Lewis <me@chrislewis.au>
2025-12-19 18:29:11 -0500

    🧑 match func_8003D210_3DE10

--- src/3CD70.c
+++ src/3CD70.c
@@ -1,11 +1,34 @@
 #include "6E840.h"
 #include "common.h"
 #include "geometry.h"
+#include "graphics.h"
 #include "task_scheduler.h"
 
-extern void func_8003C2BC_3CEBC(void);
-extern s32 D_8008FEB0_90AB0;
-extern u8 identityMatrix[];
+typedef struct {
+    s32 unk4;
+    s32 unk8;
+    s32 unkC;
+    s32 unk10;
+} CameraData;
+
+typedef struct {
+    s32 unk0;          /* 0x00 */
+    s32 unk4;          /* 0x04 */
+    s32 unk8;          /* 0x08 */
+    s32 unkC;          /* 0x0C */
+    s32 unk10;         /* 0x10 */
+    s32 unk14;         /* 0x14 */
+    CameraData *unk18; /* 0x18 */
+    CameraData *unk1C; /* 0x1C */
+    s16 unk20;         /* 0x20 */
+    s16 unk22;         /* 0x22 */
+    s8 unk24;          /* 0x24 */
+    s8 unk25;          /* 0x25 */
+} func_8003D210_3DE10_arg;
+
+typedef struct {
+    s16 unk0;
+} func_8003CF0C_arg;
 
 typedef struct {
     s32 x;        /* 0x00 */
@@ -21,8 +44,19 @@ typedef struct {
 } NodeState;
 
 typedef struct {
-    u8 pad0[0x10]; /* 0x00 */
-    u8 *players;   /* 0x10 */
+    u8 padding[0x434];
+    void *unk434;
+    u8 padding1[0xBCA - 0x438];
+    u8 unkBCA;
+    u8 padding2[0x101C - 0xBCC];
+    void *unk101C;
+    u8 padding3[0x1C04 - 0x1020];
+    void *unk1C04;
+} AllocationUnk10;
+
+typedef struct {
+    u8 pad0[0x10];
+    /* 0x10 */ AllocationUnk10 *players;
 } Allocation;
 
 typedef struct {
@@ -32,6 +66,13 @@ typedef struct {
     u8 pad14[0x65];
     u8 unk79;
 } func_8003CF40_Allocation;
+
+extern void func_8003C2BC_3CEBC(void);
+extern s32 D_8008FEB0_90AB0;
+extern u8 identityMatrix[];
+void func_8003CF40_3DB40(s16 *arg0);
+void func_8003D0F4_3DCF4(NodeExt *arg0);
+void func_8003D210_3DE10(func_8003D210_3DE10_arg *);
 
 void func_8003C170_3CD70(NodeState *arg0) {
     Allocation *allocation;
@@ -51,7 +92,7 @@ void func_8003C170_3CD70(NodeState *arg0) {
     playerIdx = *(u8 *)((u8 *)arg0 + 0xC);
 
     // Read rotation at offset 0xA94 from players[playerIdx]
-    createYRotationMatrix(&mtx, *(u16 *)(allocation->players + (playerIdx * 0xBE8) + 0xA94));
+    createYRotationMatrix(&mtx, *(u16 *)((u8 *)allocation->players + (playerIdx * 0xBE8) + 0xA94));
 
     // Transform vector
     transformVector2(tempVec, &mtx, outputVec);
@@ -60,19 +101,19 @@ void func_8003C170_3CD70(NodeState *arg0) {
     playerIdx = *(u8 *)((u8 *)arg0 + 0xC);
 
     // Set position X with transformed vector offset
-    arg0->x = *(s32 *)(allocation->players + (playerIdx * 0xBE8) + 0x434) + outputVec[0];
+    arg0->x = *(s32 *)((u8 *)allocation->players + (playerIdx * 0xBE8) + 0x434) + outputVec[0];
 
     // Read player index again
     playerIdx = *(u8 *)((u8 *)arg0 + 0xC);
 
     // Set position Y with transformed vector offset
-    arg0->y = *(s32 *)(allocation->players + (playerIdx * 0xBE8) + 0x438) + outputVec[1];
+    arg0->y = *(s32 *)((u8 *)allocation->players + (playerIdx * 0xBE8) + 0x438) + outputVec[1];
 
     // Read player index again
     playerIdx = *(u8 *)((u8 *)arg0 + 0xC);
 
     // Set position Z with transformed vector offset
-    arg0->z = *(s32 *)(allocation->players + (playerIdx * 0xBE8) + 0x43C) + outputVec[2];
+    arg0->z = *(s32 *)((u8 *)allocation->players + (playerIdx * 0xBE8) + 0x43C) + outputVec[2];
 
     // Set various node fields
     arg0->unk10 = 0x600000;
@@ -95,15 +136,6 @@ void func_8003CEC4_3DAC4(u8 arg0) {
         node->field_D = arg0;
     }
 }
-
-void func_8003D0F4_3DCF4(NodeExt *arg0);
-void func_8003D210_3DE10(void);
-
-typedef struct {
-    s16 unk0;
-} func_8003CF0C_arg;
-
-void func_8003CF40_3DB40(s16 *arg0);
 
 void func_8003CF0C_3DB0C(func_8003CF0C_arg *arg0) {
     getCurrentAllocation();
@@ -199,7 +231,57 @@ void func_8003D0F4_3DCF4(NodeExt *arg0) {
     setCallbackWithContinue(func_8003D210_3DE10);
 }
 
-INCLUDE_ASM("asm/nonmatchings/3CD70", func_8003D210_3DE10);
+void func_8003D210_3DE10(func_8003D210_3DE10_arg *arg0) {
+    Allocation *allocation;
+    u8 sp10[0x30];
+
+    allocation = getCurrentAllocation();
+
+    if (arg0->unk24 == 0) {
+        arg0->unk0 += ((arg0->unk18->unk8 - arg0->unk0) / arg0->unk20);
+        arg0->unk4 += ((arg0->unk18->unkC - arg0->unk4) / arg0->unk20);
+        arg0->unk8 += ((arg0->unk18->unk10 - arg0->unk8) / arg0->unk20);
+        arg0->unk20--;
+        if (arg0->unk20 == 0) {
+            arg0->unk18 = (CameraData *)((u8 *)arg0->unk18 + 0x10);
+            arg0->unk20 = *(s16 *)arg0->unk18;
+        }
+    }
+
+    switch (arg0->unk25) {
+        case 0:
+            memcpy(&arg0->unkC, &allocation->players->unk434, 0xC);
+            arg0->unk10 += 0x200000;
+            break;
+        case 1:
+            arg0->unkC += ((arg0->unk1C->unk8 - arg0->unkC) / arg0->unk22);
+            arg0->unk10 += ((arg0->unk1C->unkC - arg0->unk10) / arg0->unk22);
+            arg0->unk14 += ((arg0->unk1C->unk10 - arg0->unk14) / arg0->unk22);
+            arg0->unk22--;
+            if (arg0->unk22 == 0) {
+                arg0->unk1C = (CameraData *)((u8 *)arg0->unk1C + 0x10);
+                arg0->unk22 = *(s16 *)arg0->unk1C;
+            }
+            break;
+        case 2:
+            memcpy(&arg0->unkC, &allocation->players->unk1C04, 0xC);
+            arg0->unk10 += 0x200000;
+            break;
+        case 3:
+            memcpy(&arg0->unkC, &allocation->players->unk101C, 0xC);
+            arg0->unk10 += 0x200000;
+            break;
+    }
+
+    computeLookAtMatrix(arg0, &arg0->unkC, sp10);
+    func_8006FD3C_7093C(0x64, sp10);
+
+    if (allocation->players->unkBCA == 0) {
+        func_800569A4_575A4(sp10, 0);
+    } else {
+        setBufferData(sp10, 0x30, 0);
+    }
+}
 
 void func_8003D51C_3E11C(u8 arg0) {
     NodeExt *task = (NodeExt *)scheduleTask(func_8003D0F4_3DCF4, 0, 2, 0xF0);
