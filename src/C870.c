2bb9fbfdfb871ebcd56f612837a2eb340da70be1
Chris Lewis <me@chrislewis.au>
2025-12-19 22:46:24 -0500

    task-runner: fix src/C870.c:func_8000BC70_C870

--- src/C870.c
+++ src/C870.c
@@ -2,6 +2,11 @@
 #include "common.h"
 
 typedef struct {
+    /* 0x000 */ u8 buffer1[0x200];
+    /* 0x200 */ u8 buffer2[0x200];
+} IndexBuffer;
+
+typedef struct {
     DataTable_19E80 *unk0;
     void *unk4;
     void *unk8;
@@ -12,8 +17,8 @@ void func_8000BC70_C870(C870Struct *arg0, DataTable_19E80 *arg1) {
 
     getTableEntryByU16Index(arg1, 0, &output);
     arg0->unk0 = arg1;
-    arg0->unk4 = (u8 *)output.index_ptr + 0x200;
-    arg0->unk8 = output.index_ptr;
+    arg0->unk4 = ((IndexBuffer *)output.index_ptr)->buffer2;
+    arg0->unk8 = ((IndexBuffer *)output.index_ptr)->buffer1;
 }
 
 void func_8000BCC4_C8C4(C870Struct *arg0) {
