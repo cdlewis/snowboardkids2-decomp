e7beaa8d3e1d7f384dcb46ef799de45a4af14ba6
Chris Lewis <me@chrislewis.au>
2025-12-19 22:13:37 -0500

    task-runner: fix src/198B0.c:func_80018CB0_198B0

--- src/198B0.c
+++ src/198B0.c
@@ -5,19 +5,30 @@
 extern u8 D_8008D714_8E314[];
 extern u8 D_8008D6C4_8E2C4[];
 
-void func_80018D04_19904(u8 *arg0);
+typedef struct {
+    /* 0x00 */ u8 unk0;
+    /* 0x01 */ u8 unk1;
+    /* 0x02 */ u8 pad2[2];
+    /* 0x04 */ s16 unk4;
+    /* 0x06 */ s16 unk6;
+    /* 0x08 */ s16 unk8;
+    /* 0x0A */ u8 padA[2];
+    /* 0x0C */ u8 *unkC;
+} TaskData_198B0;
 
-void func_80018CB0_198B0(u8 *arg0) {
-    u8 temp = arg0[0];
-    arg0[1] = 0;
-    *(s16 *)(arg0 + 4) = 0;
-    *(s16 *)(arg0 + 6) = -0x68;
-    *(s16 *)(arg0 + 8) = 0;
-    *(u8 **)(arg0 + 0xC) = &D_8008D714_8E314[temp * 20];
+void func_80018D04_19904(TaskData_198B0 *arg0);
+
+void func_80018CB0_198B0(TaskData_198B0 *arg0) {
+    u8 temp = arg0->unk0;
+    arg0->unk1 = 0;
+    arg0->unk4 = 0;
+    arg0->unk6 = -0x68;
+    arg0->unk8 = 0;
+    arg0->unkC = &D_8008D714_8E314[temp * 20];
     setCallback(func_80018D04_19904);
 }
 
-void func_80018D04_19904(u8 *arg0) {
+void func_80018D04_19904(TaskData_198B0 *arg0) {
     GameState *state = (GameState *)getCurrentAllocation();
     s16 val;
     u8 index;
@@ -33,14 +44,14 @@ void func_80018D04_19904(u8 *arg0) {
         val -= 0x2000;
     }
 
-    index = arg0[0];
+    index = arg0->unk0;
     min = ((s16 *)D_8008D6C4_8E2C4)[index * 2];
     if (val < min) {
         max = ((s16 *)D_8008D6C4_8E2C4)[(index * 2) + 1];
         if (max < val) {
             if (((u16)(state->unk3FC - 0xC01)) < 0x7FF) {
                 state->unk424 = 1;
-                state->unk425 = arg0[0];
+                state->unk425 = arg0->unk0;
             }
         }
     }
