15d06a57f47676001c643c5a0eb5c9a16c5b0696
Chris Lewis <me@chrislewis.au>
2025-12-25 03:00:27 -0500

    🧑 match func_800BB2B0_B2870

--- src/levels/jingle_town_boss.c
+++ src/levels/jingle_town_boss.c
@@ -35,7 +35,9 @@ typedef struct {
     s32 unk434;                         /* 0x434 */
     s32 unk438;                         /* 0x438 */
     s32 unk43C;                         /* 0x43C */
-    u8 unk440[0xC];
+    s32 unk440;                         /* 0x440 */
+    s32 unk444;                         /* 0x444 */
+    s32 unk448;                         /* 0x448 */
     s32 unk44C;                         /* 0x44C */
     s32 unk450;                         /* 0x450 */
     s32 unk454;                         /* 0x454 */
@@ -45,21 +47,49 @@ typedef struct {
     s32 unk474;                         /* 0x474 */
     u8 pad478[0x950 - 0x478];
     u8 unk950[0x20];                    /* 0x950 - 0x96F */
-    u8 unk970[0x14];                    /* 0x970 - 0x983 */
-    s32 unk984;                         /* 0x984 */
-    s32 unk988;                         /* 0x988 */
-    s32 unk98C;                         /* 0x98C */
-    u8 unk990[0x20];                    /* 0x990 - 0x9AF */
-    u8 unk9B0[0x20];                    /* 0x9B0 - 0x9CF */
+    Mat3x3Padded unk970;                /* 0x970 - 0x98F */
+    Mat3x3Padded unk990;                /* 0x990 - 0x9AF */
+    Mat3x3Padded unk9B0;                /* 0x9B0 - 0x9CF */
     u8 pad9D0[0x9F0 - 0x9D0];
     u8 unk9F0[0x20];                    /* 0x9F0 - 0xA0F */
     UnkA10Entry unkA10[9];              /* 0xA10 - 0xA7B (9 * 12 = 108 = 0x6C) */
     u8 padA7C[0xA8C - 0xA7C];
     u16 unkA8C;                         /* 0xA8C */
-    u8 padA8E[0xA9C - 0xA8E];
+    u16 unkA8E;                         /* 0xA8E */
+    u16 unkA90;                         /* 0xA90 */
+    u16 unkA92;                         /* 0xA92 */
+    u16 unkA94;                         /* 0xA94 */
+    u8 padA96[0xA9C - 0xA96];
     u16 unkA9C;                         /* 0xA9C */
     u16 unkA9E;                         /* 0xA9E */
-    u8 padAA0[0xB84 - 0xAA0];
+    u8 padAA0[0x4];
+    s32 unkAA4;                         /* 0xAA4 */
+    s32 unkAA8;                         /* 0xAA8 */
+    u8 padAAC[0xAC2 - 0xAAC];
+    s16 unkAC2;                         /* 0xAC2 */
+    u8 padAC4[0xAD4 - 0xAC4];
+    s32 unkAD4;                         /* 0xAD4 */
+    s32 unkAD8;                         /* 0xAD8 */
+    s32 unkADC;                         /* 0xADC */
+    u8 padAE0[0x4];
+    s32 unkAE4;                         /* 0xAE4 */
+    s32 unkAE8;                         /* 0xAE8 */
+    s32 unkAEC;                         /* 0xAEC */
+    s32 unkAF0;                         /* 0xAF0 */
+    s32 unkAF4;                         /* 0xAF4 */
+    s32 unkAF8;                         /* 0xAF8 */
+    u8 padAFC[0xB50 - 0xAFC];
+    s32 unkB50;                         /* 0xB50 */
+    s32 unkB54;                         /* 0xB54 */
+    s32 unkB58;                         /* 0xB58 */
+    s32 unkB5C;                         /* 0xB5C */
+    u8 padB60[0xB7A - 0xB60];
+    s8 unkB7A;                          /* 0xB7A */
+    s8 unkB7B;                          /* 0xB7B */
+    u16 unkB7C;                         /* 0xB7C */
+    u16 unkB7E;                         /* 0xB7E */
+    u8 padB80[0x2];
+    u16 unkB82;                         /* 0xB82 */
     s32 unkB84;                         /* 0xB84 */
     s32 unkB88;                         /* 0xB88 */
     s32 unkB8C;                         /* 0xB8C */
@@ -75,12 +105,18 @@ typedef struct {
     u8 unkBBF;                          /* 0xBBF */
     u8 unkBC0;                          /* 0xBC0 */
     u8 unkBC1;                          /* 0xBC1 */
-    u8 padBC2[0x7];
+    u8 padBC2[0x2];
+    u8 unkBC4;                          /* 0xBC4 */
+    u8 padBC5[0x2];
+    u8 unkBC7;                          /* 0xBC7 */
+    u8 unkBC8;                          /* 0xBC8 */
     u8 unkBC9;                          /* 0xBC9 */
     u8 unkBCA;                          /* 0xBCA */
     u8 padBCB[1];
     u8 unkBCC;                          /* 0xBCC */
-    u8 padBCD[0xBDB - 0xBCD];
+    u8 padBCD[0x2];
+    u8 unkBCF;                          /* 0xBCF */
+    u8 padBD0[0xBDB - 0xBD0];
     u8 unkBDB;                          /* 0xBDB */
 } Arg0Struct;
 
@@ -92,13 +128,131 @@ typedef struct {
     s32 unk1C;
 } LocalMat;
 
+typedef s32 (*StateFunc)(void *);
+
+extern s8 D_800AB044_A23B4[];
+extern s8 D_800AB048_A23B8[];
+extern StateFunc D_800BCB20_B40E0[];
+extern s16 D_800BCB30_B40F0[];
+extern s16 D_800BCB3C_B40FC[];
+extern s32 gButtonsPressed[];
 extern FuncPtr D_800BCB5C_B411C[];
 extern FuncPtr D_800BCB74_B4134[];
 extern s16 D_800BCB7C_B413C[];
 extern u16 D_800BCB7E_B413E[];
 extern s32 D_800BCBA0_B4160[][3];
 
-INCLUDE_ASM("asm/nonmatchings/levels/jingle_town_boss", func_800BB2B0_B2870);
+extern void func_8005C838_5D438(void *);
+extern void func_800B9B90_A9A40(Player *);
+extern void func_800BC474_B3A34(Arg0Struct *);
+extern s32 getCharacterBoardStatParam0(s32, s32);
+
+void func_800BB2B0_B2870(Arg0Struct *arg0) {
+    Mat3x3Padded sp10;
+    Mat3x3Padded sp30;
+    GameState *alloc;
+    s32 temp;
+    s32 i;
+    s16 itemId;
+    s32 diff;
+
+    alloc = getCurrentAllocation();
+    if (arg0->unkBC7 != 0) {
+        func_800B9B90_A9A40((Player *)arg0);
+    } else {
+        arg0->unkB7A = D_800AB048_A23B8[arg0->unkBB8] / 4;
+        arg0->unkB7B = D_800AB044_A23B4[arg0->unkBB8] / 4;
+
+        arg0->unkB82 = arg0->unkB7C;
+        arg0->unkB7C = gButtonsPressed[arg0->unkBB8];
+        arg0->unkB7E = arg0->unkB7C & ~arg0->unkB82;
+    }
+
+    arg0->unk44C = arg0->unk434 - arg0->unk440;
+    arg0->unk450 = arg0->unk438 - arg0->unk444;
+    arg0->unk454 = arg0->unk43C - arg0->unk448;
+    memcpy(&arg0->unk440, &arg0->unk434, 0xC);
+
+    temp = distance_3d(arg0->unk434 - alloc->players->worldPosX,
+                       arg0->unk438 - alloc->players->worldPosY,
+                       arg0->unk43C - alloc->players->worldPosZ);
+
+    if ((arg0->unkBC4 == 0) && (temp > 0x1000000)) {
+        arg0->unkAA4 = getCharacterBoardStatParam0(0, 0) - 0x40000;
+    } else {
+        arg0->unkAA4 = getCharacterBoardStatParam0(0, 0) + 0x180000;
+    }
+
+    if (arg0->unkAA4 > 0x180000) {
+        arg0->unkAA4 = 0x180000;
+    }
+
+    for(i = 0; i < arg0->unkBCF; i++) {
+        arg0->unkAA4 = arg0->unkAA4 - (arg0->unkAA4 >> 2);
+    }
+
+    diff = arg0->unkAA4 - arg0->unkAA8;
+    if (diff >= 0x101) {
+        diff = 0x100;
+    }
+    if (diff < -0x80) {
+        diff = -0x80;
+    }
+    arg0->unkAA8 = arg0->unkAA8 + diff;
+
+    arg0->unkB84 &= 0xFFFBFFFF;
+
+    if (arg0->unkBBD != 3) {
+        itemId = arg0->unkAC2;
+        if (itemId != 0) {
+            if (itemId != 0x3D) {
+                if (itemId == 0x3E) {
+                    arg0->unkBBD = 2;
+                    arg0->unkBBE = 0;
+                     arg0->unkBBF = 0;
+                    arg0->unkBC0 = 0;
+                    arg0->unk474 = 0;
+                }
+            } else {
+                arg0->unkBBD = 2;
+                arg0->unkBBE = 1;
+                arg0->unkBBF = 0;
+                arg0->unkBC0 = 0;
+                arg0->unk474 = 0;
+            }
+        }
+    }
+    arg0->unkAC2 = 0;
+
+    do {
+    } while (D_800BCB20_B40E0[arg0->unkBBD](arg0) != 0);
+
+    createZRotationMatrix(&arg0->unk9B0, arg0->unkA92);
+    createCombinedRotationMatrix(&arg0->unk990, arg0->unkA8E, arg0->unkA90);
+    createYRotationMatrix(&arg0->unk970, arg0->unkA94);
+
+    func_8006B084_6BC84(&arg0->unk9B0, &arg0->unk990, &sp10);
+    func_8006B084_6BC84(&sp10, &arg0->unk970, &sp30);
+
+    sp30.unk14 -= arg0->unk970.unk14;
+    sp30.unk18 -= arg0->unk970.unk18;
+    sp30.unk1C -= arg0->unk970.unk1C;
+
+    transformVector(D_800BCB30_B40F0, (s16 *)&sp30, &arg0->unkAD4);
+    memcpy(&arg0->unkB58, &arg0->unkAD4, 0xC);
+    func_8005C838_5D438(&arg0->unkB50);
+    func_800BC474_B3A34(arg0);
+
+    transformVector(D_800BCB3C_B40FC, arg0->unk38, &arg0->unkAE4);
+    arg0->unkAE4 -= arg0->unk970.unk14;
+    arg0->unkAE8 -= arg0->unk970.unk18;
+    arg0->unkAEC -= arg0->unk970.unk1C;
+
+    transformVector(D_800BCB3C_B40FC + 6, (s16 *)arg0->unk74, &arg0->unkAF0);
+    arg0->unkAF0 -= arg0->unk970.unk14;
+    arg0->unkAF4 -= arg0->unk970.unk18;
+    arg0->unkAF8 -= arg0->unk970.unk1C;
+}
 
 INCLUDE_ASM("asm/nonmatchings/levels/jingle_town_boss", func_800BB66C_B2C2C);
 
@@ -275,7 +429,7 @@ s32 func_800BC1C0_B3780(Arg0Struct *arg0) {
         arg0->unk434 += sp10[0];
         arg0->unk438 += sp10[1];
         arg0->unk43C += sp10[2];
-        memcpy(arg0->unk440, &arg0->unk434, 0xC);
+        memcpy(&arg0->unk440, &arg0->unk434, 0xC);
         arg0->unkB84 |= 0x200000;
         transformVector((s16 *)D_800BCBA0_B4160[1], arg0->unk38, sp20);
         func_80041EA4_42AA4(sp20);
@@ -311,7 +465,7 @@ void func_800BC378_B3938(Arg0Struct *arg0) {
     u16 temp;
 
     alloc = getCurrentAllocation();
-    memcpy(&arg0->unk984, &arg0->unk434, 0xC);
+    memcpy(&arg0->unk970.unk14, &arg0->unk434, 0xC);
     allocPlus30 = &alloc->gameData;
     temp = func_80059E90_5AA90(arg0, allocPlus30, arg0->unkB94, &arg0->unk434);
     arg0->unkB94 = temp;
@@ -338,9 +492,9 @@ void func_800BC474_B3A34(Arg0Struct *arg0) {
     void *temp_s2;
     s32 *ptr;
 
-    func_8006B084_6BC84(arg0->unk990, arg0->unk970, arg0->unk9F0);
+    func_8006B084_6BC84(&arg0->unk990, &arg0->unk970, arg0->unk9F0);
     temp_s2 = arg0->unk950;
-    func_8006B084_6BC84(arg0->unk9B0, arg0->unk9F0, temp_s2);
+    func_8006B084_6BC84(&arg0->unk9B0, arg0->unk9F0, temp_s2);
 
     if (arg0->unkB88 & 0x10) {
         temp_s0 = &sp30;
@@ -388,8 +542,8 @@ void func_800BCA10_B3FD0(Arg0Struct *arg0) {
         s32 *posPtr;
         u16 temp;
 
-        arg0->unkA10[i].unk0 = arg0->unk984 + D_800BCBA0_B4160[6 + i][0];
-        arg0->unkA10[i].unk8 = arg0->unk98C + D_800BCBA0_B4160[6 + i][2];
+        arg0->unkA10[i].unk0 = arg0->unk970.unk14 + D_800BCBA0_B4160[6 + i][0];
+        arg0->unkA10[i].unk8 = arg0->unk970.unk1C + D_800BCBA0_B4160[6 + i][2];
         posPtr = &arg0->unkA10[i].unk0;
         temp = func_80059E90_5AA90(arg0, temp_s5, arg0->unkB94, posPtr);
         arg0->unkA10[i].unk4 = func_8005CFC0_5DBC0(temp_s5, temp, posPtr, 0x100000);
