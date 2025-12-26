aff4cfcea7cfdc2d4a4a8436a777f8ecf5614fd8
Chris Lewis <me@chrislewis.au>
2025-12-26 14:57:21 -0500

    🧑 match func_8002E8B4_2F4B4

--- src/2DD40.c
+++ src/2DD40.c
@@ -3,6 +3,7 @@
 #include "297B0.h"
 #include "2C8F0.h"
 #include "D_800AFE8C_A71FC_type.h"
+#include "graphics.h"
 #include "task_scheduler.h"
 
 extern void func_8000A49C_B09C(SceneModel *, s16, s16, s16, void *, s32, s8, u8, u8, s16);
@@ -12,23 +13,25 @@ extern u16 D_8008EF70_8FB70[];
 typedef struct Func2E024Arg Func2E024Arg;
 struct Func2E024Arg {
     /* 0x00 */ SceneModel *model;
-    /* 0x04 */ u8 pad0[0x14];
-    /* 0x18 */ s32 unk18;
-    /* 0x1C */ u8 pad1[0x4];
-    /* 0x20 */ s32 unk20;
-    /* 0x24 */ u8 pad2[0x1C];
+    /* 0x04 */ Mat3x3Padded unk4;
+    /* 0x24 */ u8 pad24[0x6];
+    /* 0x2A */ u16 unk2A;
+    /* 0x2C */ u16 unk2C;
+    /* 0x2E */ u8 pad2E[0x12];
     /* 0x40 */ s32 unk40;
     /* 0x44 */ s32 unk44;
     /* 0x48 */ s32 unk48;
     /* 0x4C */ u8 pad4C[0x4];
     /* 0x50 */ s16 unk50;
-    /* 0x52 */ u8 pad3b[0xC];
+    /* 0x52 */ u8 pad52[0x8];
+    /* 0x5A */ u16 unk5A;
+    /* 0x5C */ u8 pad5C[0x2];
     /* 0x5E */ u8 unk5E;
-    /* 0x5F */ u8 pad4[0x3];
+    /* 0x5F */ u8 pad5F[0x3];
     /* 0x62 */ s8 unk62;
-    /* 0x63 */ u8 pad5[0x1];
+    /* 0x63 */ u8 pad63[0x1];
     /* 0x64 */ SceneModel *unk64;
-    /* 0x68 */ u8 pad6[0x14];
+    /* 0x68 */ u8 pad68[0x14];
     /* 0x7C */ s32 unk7C;
     /* 0x80 */ u8 pad80[0x4];
     /* 0x84 */ s32 unk84;
@@ -38,17 +41,17 @@ struct Func2E024Arg {
     /* 0xAC */ s32 unkAC;
     /* 0xB0 */ u8 padB0[0x4];
     /* 0xB4 */ s16 unkB4;
-    /* 0xB6 */ u8 pad8[0x8];
+    /* 0xB6 */ u8 padB6[0x8];
     /* 0xBE */ s16 unkBE;
-    /* 0xC0 */ u8 pad9[0x2];
+    /* 0xC0 */ u8 padC0[0x2];
     /* 0xC2 */ u8 unkC2;
-    /* 0xC3 */ u8 pad10[0x3];
+    /* 0xC3 */ u8 padC3[0x3];
     /* 0xC6 */ s8 unkC6;
-    /* 0xC7 */ u8 pad11[0x1];
+    /* 0xC7 */ u8 padC7[0x1];
     /* 0xC8 */ void (*unkC8)(Func2E024Arg *);
     /* 0xCC */ s16 unkCC;
     /* 0xCE */ s16 unkCE;
-    /* 0xD0 */ u8 pad12[0x5];
+    /* 0xD0 */ u8 padD0[0x5];
     /* 0xD5 */ u8 unkD5;
 };
 
@@ -139,8 +142,8 @@ void func_8002DE44_2EA44(Func2E024Arg *arg0) {
         ((Func2E024Arg *)&elements[i])->unk62 = 0;
 
         if (i == 0) {
-            arg0->unk18 = 0xFFFC0000;
-            arg0->unk20 = 0xFFBC0000;
+            arg0->unk4.unk14 = 0xFFFC0000;
+            arg0->unk4.unk1C = 0xFFBC0000;
             func_80001688_2288(arg0->model, 4);
             arg0->unk50 = 0x11;
             arg0->unk5E = 0x63;
@@ -221,7 +224,96 @@ INCLUDE_ASM("asm/nonmatchings/2DD40", func_8002E314_2EF14);
 
 INCLUDE_ASM("asm/nonmatchings/2DD40", func_8002E680_2F280);
 
-INCLUDE_ASM("asm/nonmatchings/2DD40", func_8002E8B4_2F4B4);
+void func_8002E8B4_2F4B4(Func2E024Arg *arg0) {
+    GameState *allocation;
+    s32 incrementedCount;
+    s32 i;
+    Func2E024Arg *ptr;
+    s32 sp10[3];
+    s32 sp20[3];
+    Mat3x3Padded sp30;
+    Mat3x3Padded sp50;
+    s16 var_v1;
+    s32 var_a0;
+    s32 tempAngle;
+    s32 count;
+
+    allocation = getCurrentAllocation();
+    incrementedCount = 0;
+
+    for (i = 0; i < arg0->unkD5; i++) {
+        ptr = (Func2E024Arg *)((u8 *)arg0 + i * 0x64);
+
+        memcpy(&sp30, identityMatrix, sizeof(Mat3x3Padded));
+        memcpy(&sp50, &ptr->unk4, sizeof(Mat3x3Padded));
+
+        switch (ptr->unk5E) {
+            case 0:
+                ptr->unk5A--;
+                if ((ptr->unk5A << 16) == 0) {
+                    ptr->unk5E = 1;
+                }
+                break;
+            case 1:
+                memcpy(sp20, &ptr->unk4.unk14, 0xC);
+                memcpy(sp10, sp20, 0xC);
+
+                if (i == 0) {
+                    var_a0 = 0x5200;
+                    var_v1 = -0x28;
+                } else {
+                    var_a0 = 0x4400;
+                    var_v1 = -0x18;
+                }
+
+                sp10[0] += ((var_v1 * (sp20[2] >> 8)) / var_a0) << 12;
+                sp10[2] += ((-var_v1 * (sp20[0] >> 8)) / var_a0) << 12;
+                memcpy(&ptr->unk4.unk14, sp10, 0xC);
+                break;
+            case 2:
+                incrementedCount++;
+                break;
+        }
+
+        {
+            u16 temp = arg0->unkCC;
+            if (temp != 0) {
+                temp--;
+                arg0->unkCC = temp;
+                if ((temp & 0xFFFF) == 0) {
+                    func_800585C8_591C8(0xF5);
+                }
+            }
+        }
+
+        {
+            u16 temp = arg0->unkCE;
+            if (temp != 0) {
+                temp--;
+                arg0->unkCE = temp;
+                if ((temp & 0xFFFF) == 0) {
+                    func_800585C8_591C8(0x10E);
+                }
+            }
+        }
+
+        if (ptr->unk5E != 2) {
+            createYRotationMatrix(&sp30, ptr->unk2C);
+            tempAngle = atan2Fixed(ptr->unk4.unk14, ptr->unk4.unk1C);
+            createYRotationMatrix(&sp50, tempAngle & 0xFFFF);
+            memcpy(&sp50.unk14, &ptr->unk4.unk14, 0xC);
+            func_8006B084_6BC84(&sp30, &sp50, &ptr->unk4);
+            func_8002A2D0_2AED0((Func297D8Arg *)ptr);
+            allocation->unk408[i] = ptr->unk4.unk14;
+            allocation->unk410[i] = ptr->unk4.unk1C;
+            if (((u32)((tempAngle - 0x1001) & 0xFFFF) < 0x468)) {
+                ptr->unk5E = 2;
+            }
+        } else if ((incrementedCount & 0xFF) == ptr->unk5E) {
+            func_80069CF8_6A8F8();
+        }
+    }
+}
 
 extern u8 D_8008F044_8FC44[];
 extern s8 D_8008F054_8FC54[];
