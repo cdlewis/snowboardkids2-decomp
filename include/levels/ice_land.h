2b0014c64de2fdecaedc6a24e52f539a33feb2bf
Chris Lewis <me@chrislewis.au>
2026-01-16 18:57:00 -0500

    rename func_800BB2B0_B2030 to initIceLandMovingPlatform and rename related symbols
    - Rename func_800BB2B0_B2030 to initIceLandMovingPlatform
    - Rename func_800BB650_B23D0 to cleanupIceLandMovingPlatform
    - Rename func_800BB6B4_B2434 to scheduleIceLandMovingPlatform
    - Rename B2030Payload to IceLandMovingPlatformTask
    - Rename B2030TaskPayload to IceLandMovingPlatformSchedulerTask
    - Add IceLandMovingPlatformCleanupArgs struct for cleanup callback
    - Rename WaypointEntry fields: unk0->isLastWaypoint, unk4->x, unk8->y, unkC->z
    - Rename IceLandMovingPlatformTask fields:
      - unk20->displayLists
      - unk24/unk28->segment1/segment2
      - unk5C->waypoints
      - index->currentWaypointIndex
    - Rename IceLandMovingPlatformSchedulerTask fields: unk0->timer, unk2->pathIndex

--- include/levels/ice_land.h
+++ include/levels/ice_land.h
@@ -3,11 +3,17 @@
 #include "common.h"
 
 typedef struct {
-    s16 unk0;
-    s16 unk2;
-    u8 pad4[0x20];
-    void *unk24;
-    void *unk28;
-} B2030TaskPayload;
+    u8 pad[0x24];
+    void *segment1;
+    void *segment2;
+} IceLandMovingPlatformCleanupArgs;
 
-void func_800BB688_B2408(B2030TaskPayload *arg0);
+typedef struct {
+    s16 timer;
+    s16 pathIndex;
+    u8 pad4[0x20];
+    void *segment1;
+    void *segment2;
+} IceLandMovingPlatformSchedulerTask;
+
+void func_800BB688_B2408(IceLandMovingPlatformSchedulerTask *arg0);
--- src/levels/ice_land.c
+++ src/levels/ice_land.c
@@ -6,11 +6,11 @@
 #include "task_scheduler.h"
 
 typedef struct {
-    u8 unk0;
+    u8 isLastWaypoint;
     u8 pad1[3];
-    s32 unk4;
-    s32 unk8;
-    s32 unkC;
+    s32 x;
+    s32 y;
+    s32 z;
 } WaypointEntry;
 
 typedef struct {
@@ -20,22 +20,22 @@ typedef struct {
 
 typedef struct {
     Transform3D unk0;
-    DisplayLists *unk20;
-    void *unk24;
-    void *unk28;
+    DisplayLists *displayLists;
+    void *segment1;
+    void *segment2;
     s32 unk2C;
     u8 pad30[0xC];
     s16 matrix3C[9];
     u8 pad4E[2];
     s32 position50[3];
-    WaypointEntry *unk5C;
+    WaypointEntry *waypoints;
     u8 pad60[4];
     s16 angleX;
     s16 angleY;
     s16 unk68;
-    s16 index;
+    s16 currentWaypointIndex;
     s16 unk6C;
-} B2030Payload;
+} IceLandMovingPlatformTask;
 
 typedef struct {
     u8 pad0[0x5C];
@@ -87,60 +87,60 @@ WaypointEntry iceLandWaypoints3[] = {
 s32 D_800BB9C0_B2740[] = { 0, 0, 0x00080000, 0 };
 
 void func_800BB3A0_B2120(void);
-void func_800BB650_B23D0(B2030TaskPayload *arg0);
-void func_800BB6B4_B2434(B2030TaskPayload *arg0);
+void cleanupIceLandMovingPlatform(IceLandMovingPlatformCleanupArgs *arg0);
+void scheduleIceLandMovingPlatform(IceLandMovingPlatformSchedulerTask *arg0);
 
-void func_800BB2B0_B2030(B2030Payload *arg0) {
+void initIceLandMovingPlatform(IceLandMovingPlatformTask *arg0) {
     Allocation *allocation;
     WaypointEntry *waypoints;
     unsigned int angle;
     allocation = getCurrentAllocation();
-    arg0->unk20 = &getSkyDisplayLists3ByIndex(allocation->memoryPoolId)->sceneryDisplayLists1;
-    arg0->unk24 = loadUncompressedAssetByIndex(allocation->memoryPoolId);
-    arg0->unk28 = loadCompressedSegment2AssetByIndex(allocation->memoryPoolId);
-    arg0->index = 0;
+    arg0->displayLists = &getSkyDisplayLists3ByIndex(allocation->memoryPoolId)->sceneryDisplayLists1;
+    arg0->segment1 = loadUncompressedAssetByIndex(allocation->memoryPoolId);
+    arg0->segment2 = loadCompressedSegment2AssetByIndex(allocation->memoryPoolId);
+    arg0->currentWaypointIndex = 0;
     arg0->unk2C = 0;
     arg0->unk6C = 0;
-    memcpy(&arg0->position50, &(arg0->unk5C - (-arg0->index))->unk4, 12);
-    arg0->index++;
-    waypoints = arg0->unk5C;
+    memcpy(&arg0->position50, &(arg0->waypoints - (-arg0->currentWaypointIndex))->x, 12);
+    arg0->currentWaypointIndex++;
+    waypoints = arg0->waypoints;
     arg0->angleX = 0;
     angle = computeAngleToPosition(
-        waypoints[arg0->index].unk4,
-        waypoints[arg0->index].unkC,
+        waypoints[arg0->currentWaypointIndex].x,
+        waypoints[arg0->currentWaypointIndex].z,
         arg0->position50[0],
         arg0->position50[2]
     );
     arg0->angleY = angle & 0x1FFF;
     createYRotationMatrix(&arg0->unk0, arg0->angleY);
     createCombinedRotationMatrix(&arg0->matrix3C, arg0->angleX, arg0->angleY);
-    setCleanupCallback(func_800BB650_B23D0);
+    setCleanupCallback(cleanupIceLandMovingPlatform);
     setCallback(func_800BB3A0_B2120);
 }
 
 INCLUDE_ASM("asm/nonmatchings/levels/ice_land", func_800BB3A0_B2120);
 
-void func_800BB650_B23D0(B2030TaskPayload *arg0) {
-    arg0->unk24 = freeNodeMemory(arg0->unk24);
-    arg0->unk28 = freeNodeMemory(arg0->unk28);
+void cleanupIceLandMovingPlatform(IceLandMovingPlatformCleanupArgs *arg0) {
+    arg0->segment1 = freeNodeMemory(arg0->segment1);
+    arg0->segment2 = freeNodeMemory(arg0->segment2);
 }
 
-void func_800BB688_B2408(B2030TaskPayload *arg0) {
-    arg0->unk0 = 0x78;
-    arg0->unk2 = 0;
-    setCallback(func_800BB6B4_B2434);
+void func_800BB688_B2408(IceLandMovingPlatformSchedulerTask *arg0) {
+    arg0->timer = 0x78;
+    arg0->pathIndex = 0;
+    setCallback(scheduleIceLandMovingPlatform);
 }
 
-void func_800BB6B4_B2434(B2030TaskPayload *arg0) {
+void scheduleIceLandMovingPlatform(IceLandMovingPlatformSchedulerTask *arg0) {
     GameState *allocation;
     ScheduledTask *task;
 
     allocation = (GameState *)getCurrentAllocation();
     if (allocation->gamePaused == 0) {
-        if (arg0->unk0 == 0) {
-            task = (ScheduledTask *)scheduleTask(func_800BB2B0_B2030, 0, 0, 0xD3);
+        if (arg0->timer == 0) {
+            task = (ScheduledTask *)scheduleTask(initIceLandMovingPlatform, 0, 0, 0xD3);
             if (task != NULL) {
-                switch (arg0->unk2) {
+                switch (arg0->pathIndex) {
                     case 0:
                         task->waypoints = iceLandWaypoints1;
                         break;
@@ -151,14 +151,14 @@ void func_800BB6B4_B2434(B2030TaskPayload *arg0) {
                         task->waypoints = iceLandWaypoints3;
                         break;
                 }
-                arg0->unk2++;
-                if (arg0->unk2 >= 3) {
-                    arg0->unk2 = 0;
+                arg0->pathIndex++;
+                if (arg0->pathIndex >= 3) {
+                    arg0->pathIndex = 0;
                 }
             }
-            arg0->unk0 = 0x78;
+            arg0->timer = 0x78;
         } else {
-            arg0->unk0--;
+            arg0->timer--;
         }
     }
 }
