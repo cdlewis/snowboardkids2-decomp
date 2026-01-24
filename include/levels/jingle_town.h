f00df8ebeadb8bbc3b8e56ffd25d7a2c26f096c6
Chris Lewis <me@chrislewis.au>
2026-01-24 13:47:27 -0500

    reorganize src/levels/jingle_town.c structure
    - Move extern declarations for train data arrays to header file
    - Move struct definitions (B4240Struct, B4240AllocationStruct, JingleTownTrain) to header file
    - Add function declarations to header file
    - Reorganize includes to be alphabetical and include the module header
    - Remove redundant local declarations that are now in the header

--- include/levels/jingle_town.h
+++ include/levels/jingle_town.h
@@ -1,5 +1,53 @@
 #pragma once
 
 #include "common.h"
+#include "displaylist.h"
+#include "geometry.h"
+
+/* Global data arrays for train positioning */
+extern s32 gJingleTownTrainInitialHeights[];
+extern s32 gJingleTownTrainWaypointsX[];
+extern s32 gJingleTownTrainWaypointsZ[];
+extern s32 gJingleTownTrainWaypointsX2[];
+extern s32 gJingleTownTrainWaypointsZ2[];
+extern Vec3i gJingleTownTrainForwardVector;
+extern Vec3i gJingleTownTrainForwardVector2;
+
+typedef struct {
+    u8 _pad0[0x24];
+    void *unk24;
+    void *unk28;
+} B4240Struct;
+
+typedef struct {
+    u8 _pad0[0x30];
+    void *unk30;
+    u8 _pad34[0x28];
+    u8 unk5C;
+    u8 _pad5D[0x19];
+    u8 unk76;
+} B4240AllocationStruct;
+
+typedef struct {
+    DisplayListObject node;
+    s32 posX;
+    s32 height;
+    s32 posZ;
+    s32 yOffset;
+    s32 yVelocity;
+    s16 trainIndex;
+    s16 waypointIndex;
+    u16 rotation;
+    s16 unk56;
+    s16 waitTimer;
+    s16 loopCount;
+} JingleTownTrain;
 
 void spawnJingleTownTrains(void);
+void renderJingleTownTrain(JingleTownTrain *train);
+void pullPlayersToTrain(JingleTownTrain *train);
+void initJingleTownTrain(JingleTownTrain *train);
+void handleTrainIdleState(JingleTownTrain *train);
+void handleTrainHopBehavior(JingleTownTrain *train);
+void handleTrainJumpBehavior(JingleTownTrain *train);
+void cleanupJingleTownTrain(B4240Struct *arg0);
