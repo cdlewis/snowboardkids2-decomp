51ca12420ba5da8cc71e3cb5a902484b7753359c
Chris Lewis <me@chrislewis.au>
2026-02-01 19:23:17 -0500

    🧑 match func_80005094_5C94

--- src/5520.c
+++ src/5520.c
@@ -54,12 +54,15 @@ void renderOpaqueTiledTexture(DisplayListObject *arg0);
 void renderTransparentTiledTexture(s32 arg0);
 void renderOverlayTiledTexture(s32 arg0);
 void func_80004920_5520(void);
-void func_80005094_5C94(void);
+void func_80005094_5C94(ScrollingTextureState *);
 void cleanupRotatingLogo(RotatingLogoState *);
 void func_8000595C_655C(void);
 void *loadAssetGroupDisplayList(void *);
 void *loadAssetGroupVertexData(void *);
 void *loadAssetGroupSoundData(void *);
+
+extern s16 gGraphicsMode;
+extern Gfx *gRegionAllocPtr;
 
 INCLUDE_ASM("asm/nonmatchings/5520", func_80004920_5520);
 
@@ -94,20 +97,114 @@ void enqueueScrollingTextureRender(u16 renderLayer, DisplayListObject *displayLi
     }
 }
 
-INCLUDE_ASM("asm/nonmatchings/5520", func_80005094_5C94);
+void func_80005094_5C94(ScrollingTextureState *arg0) {
+    OutputStruct_19E80 tableEntry;
+    s32 tempWidth;
+    s32 tempHeight;
+    s32 widthShift;
+    s32 heightShift;
+    void *paletteAddr;
+
+    gDPPipeSync(gRegionAllocPtr++);
+    gDPSetTextureLUT(gRegionAllocPtr++, G_TT_RGBA16);
+    gGraphicsMode = -1;
+
+    getTableEntryByU16Index(arg0->unk3C, arg0->unk4C, &tableEntry);
+
+    do {
+        tempWidth = tableEntry.field1;
+        widthShift = 0;
+    loop_1:
+        if (!(tempWidth & 1)) {
+            widthShift += 1;
+            if (widthShift < 16) {
+                tempWidth >>= 1;
+                goto loop_1;
+            }
+        }
+
+        tempHeight = tableEntry.field2;
+        heightShift = 0;
+    loop_2:
+        if (!(tempHeight & 1)) {
+            heightShift += 1;
+            if (heightShift < 16) {
+                tempHeight >>= 1;
+                goto loop_2;
+            }
+        }
+    } while (0);
+
+    if (arg0->unk4E == 0) {
+        paletteAddr = (void *)((u8 *)tableEntry.index_ptr + (arg0->unk50 << 5));
+
+        gDPLoadTextureBlock_4b(
+            gRegionAllocPtr++,
+            tableEntry.data_ptr,
+            G_IM_FMT_CI,
+            tableEntry.field1,
+            tableEntry.field2,
+            0,
+            0,
+            0,
+            widthShift,
+            heightShift,
+            0,
+            0
+        );
+
+        gDPSetTileSize(
+            gRegionAllocPtr++,
+            G_TX_RENDERTILE,
+            arg0->unk44,
+            arg0->unk46,
+            ((tableEntry.field1 + (s16)arg0->unk44 - 1) << 2),
+            ((tableEntry.field2 + (s16)arg0->unk46 - 1) << 2)
+        );
+
+        gDPLoadTLUT_pal16(gRegionAllocPtr++, 0, paletteAddr);
+    } else {
+        gDPLoadTextureBlock(
+            gRegionAllocPtr++,
+            tableEntry.data_ptr,
+            G_IM_FMT_CI,
+            G_IM_SIZ_8b,
+            tableEntry.field1,
+            tableEntry.field2,
+            0,
+            0,
+            0,
+            widthShift,
+            heightShift,
+            0,
+            0
+        );
+
+        gDPSetTileSize(
+            gRegionAllocPtr++,
+            G_TX_RENDERTILE,
+            arg0->unk44,
+            arg0->unk46,
+            ((tableEntry.field1 + (s16)arg0->unk44 - 1) << 2),
+            ((tableEntry.field2 + (s16)arg0->unk46 - 1) << 2)
+        );
+
+        gDPLoadTLUT_pal256(gRegionAllocPtr++, tableEntry.index_ptr);
+    }
+}
 
 void renderOpaqueTiledTexture(DisplayListObject *arg0) {
-    func_80005094_5C94();
+    func_80005094_5C94((ScrollingTextureState *)arg0);
     buildDisplayListSegment(arg0);
 }
 
 void renderTransparentTiledTexture(s32 arg0) {
-    func_80005094_5C94();
+    func_80005094_5C94((ScrollingTextureState *)arg0);
     buildTransparentDisplayListSegment((DisplayListObject *)arg0);
 }
 
 void renderOverlayTiledTexture(s32 arg0) {
-    func_80005094_5C94();
+    func_80005094_5C94((ScrollingTextureState *)arg0);
     buildOverlayDisplayListSegment((DisplayListObject *)arg0);
 }
 
