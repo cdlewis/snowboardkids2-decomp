dcf66675acff6f8b84feb1b5e293601d252d9c2f
Chris Lewis <me@chrislewis.au>
2026-06-07 00:29:49 -0400

    Make some cutscene data public

--- include/effects/cutscene_keyframes.h
+++ include/effects/cutscene_keyframes.h
@@ -27,6 +27,40 @@ typedef struct {
     ScreenTransitionState transitionState;
 } CutsceneTaskMemory;
 
+typedef union {
+    struct {
+        s16 lower;
+        s16 upper;
+    } half;
+    s32 full;
+} CompositeInt;
+
+typedef struct {
+    s16 slotIndex;
+    s16 cutsceneType;
+    s16 frameIndex;
+    s16 unk6;
+    CompositeInt slotSelection;
+    CompositeInt typeSelection;
+} CutsceneState;
+
+typedef enum {
+    CUTSCENE_STATE_INIT_DMA,
+    CUTSCENE_STATE_WAIT_DMA,
+    CUTSCENE_STATE_INIT_MANAGER,
+    CUTSCENE_STATE_START_PLAYBACK,
+    CUTSCENE_STATE_DELAY,
+    CUTSCENE_STATE_PLAYING,
+    CUTSCENE_STATE_SKIP_START,
+    CUTSCENE_STATE_SKIP_FADE,
+    CUTSCENE_STATE_TERMINATE_TASKS,
+    CUTSCENE_STATE_WAIT_TERMINATE,
+    CUTSCENE_STATE_CLEANUP,
+    CUTSCENE_STATE_EXIT
+} CutsceneStateEnum;
+
+extern CutsceneState gCutsceneState;
+
 void *__udiv_w_sdiv(void);
 void loadCutsceneOverlay(void);
 void setCutsceneSelection(s16 slotIndex, s16 cutsceneType);
--- src/effects/cutscene_keyframes.c
+++ src/effects/cutscene_keyframes.c
@@ -14,38 +14,6 @@
 
 USE_OVERLAY(cutscene);
 
-typedef union {
-    struct {
-        s16 lower;
-        s16 upper;
-    } half;
-    s32 full;
-} CompositeInt;
-
-typedef struct {
-    s16 slotIndex;
-    s16 cutsceneType;
-    s16 frameIndex;
-    s16 unk6;
-    CompositeInt slotSelection;
-    CompositeInt typeSelection;
-} CutsceneState;
-
-typedef enum {
-    CUTSCENE_STATE_INIT_DMA,
-    CUTSCENE_STATE_WAIT_DMA,
-    CUTSCENE_STATE_INIT_MANAGER,
-    CUTSCENE_STATE_START_PLAYBACK,
-    CUTSCENE_STATE_DELAY,
-    CUTSCENE_STATE_PLAYING,
-    CUTSCENE_STATE_SKIP_START,
-    CUTSCENE_STATE_SKIP_FADE,
-    CUTSCENE_STATE_TERMINATE_TASKS,
-    CUTSCENE_STATE_WAIT_TERMINATE,
-    CUTSCENE_STATE_CLEANUP,
-    CUTSCENE_STATE_EXIT
-} CutsceneStateEnum;
-
 CutsceneState gCutsceneState = { .slotIndex = 0,
                                  .cutsceneType = 0,
                                  .frameIndex = 0,
